4ac512463f17615636a8603f319cc43a3837d6d9
Adam Grayson <agrayson@xmog.com>
2022-02-22 09:08:11 -0500

    clean up date logic stuff

--- src/pages/scheduling/edit-availability-panel.tsx
+++ src/pages/scheduling/edit-availability-panel.tsx
@@ -26,17 +26,17 @@ export const EditAvailabilityPanel = ({ logicalAvailabilityId, onClose }: EditAv
 		}
 
 		const { logicalAvailability } = await schedulingService.getLogicalAvailability(logicalAvailabilityId).fetch();
-		const startMoment = moment(logicalAvailability.startDateTime, 'YYYY-MM-DDTHH:mm').toISOString();
-		const endMoment = moment(logicalAvailability.endDateTime, 'YYYY-MM-DDTHH:mm').toISOString();
+		const startMoment = moment(logicalAvailability.startDateTime, 'YYYY-MM-DDTHH:mm');
+		const endMoment = moment(logicalAvailability.endDateTime, 'YYYY-MM-DDTHH:mm');
 
 		setInitialValues({
 			appointmentTypes: logicalAvailability.appointmentTypes.map((at) => at.appointmentTypeId),
-			startDate: moment(startMoment).format('MMM DD, yyyy'),
-			startTime: moment(startMoment).format('hh:mm'),
-			startTimeMeridian: moment(startMoment).format('a'),
-			endDate: moment(endMoment).format('MMM DD, yyyy'),
-			endTime: moment(endMoment).format('hh:mm'),
-			endTimeMeridian: moment(endMoment).format('a'),
+			startDate: startMoment.format('MMM DD, yyyy'),
+			startTime: startMoment.format('hh:mm'),
+			startTimeMeridian: startMoment.format('a'),
+			endDate: endMoment.format('MMM DD, yyyy'),
+			endTime: endMoment.format('hh:mm'),
+			endTimeMeridian: endMoment.format('a'),
 			typesAccepted: logicalAvailability.appointmentTypes.length > 0 ? 'limited' : 'all',
 			recurring: logicalAvailability.recurrenceTypeId === 'DAILY' ? true : false,
 			occurance: {
