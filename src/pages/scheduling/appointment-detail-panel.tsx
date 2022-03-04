efbb617886f91c4fcc3358ce34e216ac78a3487b
Khaled Shaaban <khaled.shaaban@xmog.com>
2022-03-04 08:55:11 -0500

    extract components to separate files

---
+++ src/pages/scheduling/appointment-detail-panel.tsx
@@ -0,0 +1,154 @@
+import useHandleError from '@/hooks/use-handle-error';
+import { AccountModel, AppointmentModel } from '@/lib/models';
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { ReactComponent as EditIcon } from '@/assets/icons/edit.svg';
+import { ReactComponent as CloseIcon } from '@/assets/icons/icon-close.svg';
+import { ReactComponent as PlusIcon } from '@/assets/icons/icon-plus.svg';
+import { appointmentService } from '@/lib/services';
+import { ERROR_CODES } from '@/lib/http-client';
+import { CopyToClipboardButton } from './copy-to-clipboard-button';
+import { useSchedulingStyles } from './use-scheduling-styles';
+
+interface AppointmentDetailPanelProps {
+	appointmentId?: string;
+	onEdit: () => void;
+	onClose: () => void;
+	onAddAppointment: () => void;
+}
+
+export const AppointmentDetailPanel = ({
+	appointmentId,
+	onEdit,
+	onClose,
+	onAddAppointment,
+}: AppointmentDetailPanelProps) => {
+	const schedulingClasses = useSchedulingStyles();
+	const handleError = useHandleError();
+	const [patient, setPatient] = useState<AccountModel>();
+	const [appointment, setAppointment] = useState<AppointmentModel>();
+
+	useEffect(() => {
+		const request = appointmentService.getAppointment(appointmentId);
+
+		request
+			.fetch()
+			.then((response) => {
+				setPatient(response.account);
+				setAppointment(response.appointment);
+			})
+			.catch((e) => {
+				if (e.code !== ERROR_CODES.REQUEST_ABORTED) {
+					handleError(e);
+				}
+			});
+
+		return () => {
+			request.abort();
+		};
+	}, [appointmentId, handleError]);
+
+	return (
+		<div>
+			<div className="d-flex align-items-center justify-content-between py-4">
+				<h4>
+					{patient?.firstName} {patient?.lastName}
+				</h4>
+
+				<Button variant="link" size="sm" className="p-0" onClick={() => onClose()}>
+					<CloseIcon />
+				</Button>
+			</div>
+
+			<div className="mb-4">
+				<Button as="a" variant="primary" size="sm" className="mr-1" href={appointment?.videoconferenceUrl}>
+					join now
+				</Button>
+
+				<CopyToClipboardButton className="mr-1" text={appointment?.videoconferenceUrl} />
+
+				<Button variant="primary" size="sm" className="px-2" onClick={onEdit}>
+					<EditIcon />
+				</Button>
+			</div>
+
+			<div className="border p-2 my-2">
+				<div className="d-flex justify-content-between align-items-center">
+					<p className="mb-0">
+						<strong>contact information</strong>
+					</p>
+
+					<Button
+						variant="link"
+						size="sm"
+						className="p-0"
+						onClick={() => {
+							return;
+						}}
+					>
+						<EditIcon />
+					</Button>
+				</div>
+			</div>
+
+			<div className="border p-2 my-2">
+				<div className="d-flex mb-1 justify-content-between align-items-center">
+					<p className="mb-0">
+						<strong>assessments completed</strong>
+					</p>
+				</div>
+
+				<div className="d-flex mb-1 justify-content-between align-items-center">
+					<p className="mb-0">
+						<strong>PH-9</strong> Mon 7/28/20
+					</p>
+
+					<Button
+						variant="link"
+						size="sm"
+						className="p-0"
+						onClick={() => {
+							alert('TODO: Show assessment results');
+						}}
+					>
+						view
+					</Button>
+				</div>
+
+				<div className="d-flex mb-1 justify-content-between align-items-center">
+					<p className="mb-0">
+						<strong>PH-9</strong> Wed 8/5/20
+					</p>
+
+					<Button
+						variant="link"
+						size="sm"
+						className="p-0"
+						onClick={() => {
+							alert('TODO: Show assessment results');
+						}}
+					>
+						view
+					</Button>
+				</div>
+			</div>
+
+			<div className="border p-2 my-2">
+				<div className="d-flex justify-content-between align-items-center">
+					<p className="mb-0">
+						<strong>all appointments</strong>
+					</p>
+
+					<button
+						className={schedulingClasses.roundBtn}
+						onClick={() => {
+							onAddAppointment();
+						}}
+					>
+						<PlusIcon />
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
---
+++ src/pages/scheduling/use-scheduling-styles.tsx
@@ -0,0 +1,237 @@
+import colors from '@/jss/colors';
+import fonts from '@/jss/fonts';
+import { createUseStyles } from 'react-jss';
+
+export const useSchedulingStyles = createUseStyles({
+	roundBtn: {
+		width: 36,
+		height: 36,
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		borderRadius: 100,
+		border: `2px solid ${colors.primary}`,
+		backgroundColor: 'transparent',
+		'& path': {
+			fill: colors.primary,
+		},
+	},
+	typeahead: {
+		'& .rbt input': {
+			height: 56,
+		},
+		'& .rbt .rbt-menu': {
+			border: `1px solid ${colors.border}`,
+			padding: 8,
+			...fonts.xs,
+		},
+		'& .dropdown-item': {
+			textDecoration: 'none',
+		},
+	},
+});
+export const useContainerStyles = createUseStyles({
+	wrapper: {
+		display: 'flex',
+		height: 'calc(100vh - 60px)', // subtracting header + footer height
+	},
+	sideBar: {
+		width: 440,
+		flexShrink: 0,
+		backgroundColor: colors.white,
+		overflowX: 'scroll',
+	},
+	blockedTimeslot: {
+		background: `repeating-linear-gradient(
+			-45deg,
+			transparent,
+			transparent 9px,
+			${colors.black} 10px,
+			${colors.black} 11px
+		) !important;`,
+	},
+	blockedAvailabilityTimeslot: {
+		background: `repeating-linear-gradient(
+			-45deg,
+			#6C7978,
+			#6C7978 9px,
+			${colors.black} 10px,
+			${colors.black} 11px
+		) !important;`,
+	},
+	leftCalendar: {
+		'& .fc .fc-toolbar.fc-header-toolbar': {
+			marginBottom: 12,
+			'& .fc-toolbar-chunk': {
+				display: 'flex',
+				alignItems: 'center',
+				'& .fc-toolbar-title': {
+					...fonts.m,
+				},
+				'& button.fc-prev-button, & button.fc-next-button': {
+					margin: 0,
+					border: 0,
+					width: 32,
+					height: 32,
+					padding: 0,
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					backgroundColor: 'transparent',
+					'& .fc-icon': {
+						color: colors.dark,
+					},
+				},
+				'& button.fc-prev-button': {
+					marginLeft: 8,
+				},
+				'& button.fc-today-button': {
+					...fonts.xs,
+					borderRadius: 500,
+					padding: '4px 12px',
+					color: colors.primary,
+					backgroundColor: 'transparent',
+					border: `1px solid ${colors.gray200}`,
+				},
+			},
+		},
+		'& .fc-theme-standard .fc-scrollgrid': {
+			border: 0,
+		},
+		'& .fc-theme-standard td, .fc-theme-standard th': {
+			border: 0,
+			'& a:not([href])': {
+				...fonts.xs,
+				color: colors.gray600,
+			},
+		},
+		'& .fc .fc-daygrid-day-bg .fc-bg-event': {
+			borderRadius: 50,
+		},
+		'& .fc .fc-daygrid-day': {
+			'& .fc-daygrid-day-frame': {
+				width: 45,
+				height: 45,
+				display: 'flex',
+				borderRadius: '50%',
+				alignItems: 'center',
+				flexDirection: 'column',
+				justifyContent: 'center',
+				margin: '0 auto',
+			},
+			'& .fc-daygrid-day-number': {
+				padding: 0,
+				marginTop: -4,
+				color: colors.dark,
+			},
+			'& .fc-daygrid-day-events': {
+				width: 6,
+				height: 6,
+				margin: 0,
+				minHeight: 0,
+				flexShrink: 0,
+				borderRadius: '50%',
+				position: 'relative',
+				border: `1px solid ${colors.gray600}`,
+				'& .fc-daygrid-event-harness': {
+					width: 6,
+					height: 6,
+					top: -1,
+					left: -1,
+					borderRadius: '50%',
+					position: 'relative',
+					backgroundColor: colors.primary,
+					'& .fc-daygrid-event': {
+						display: 'none',
+					},
+				},
+				'& .fc-daygrid-event-harness:not(:first-of-type)': {
+					display: 'none',
+				},
+			},
+			'&.fc-day-today': {
+				backgroundColor: 'transparent',
+				'& .fc-daygrid-day-frame': {
+					backgroundColor: colors.secondary,
+				},
+				'& .fc-daygrid-day-number': {
+					color: colors.white,
+				},
+				'& .fc-daygrid-day-events': {
+					border: `1px solid ${colors.white}`,
+					'& .fc-daygrid-event-harness': {
+						backgroundColor: colors.white,
+					},
+				},
+			},
+		},
+	},
+	mainCalendar: {
+		flex: 1,
+		height: '100%',
+		'& .fc': {
+			'& .fc-col-header-cell-cushion': {
+				...fonts.s,
+				color: colors.dark,
+				padding: '9px 7px',
+			},
+			'& .fc-daygrid-day-events': {
+				margin: 0,
+			},
+			'& .fc-daygrid-body-natural .fc-daygrid-day-events': {
+				margin: 0,
+			},
+			'& .fc-daygrid-event-harness > a': {
+				margin: 0,
+				borderRadius: 0,
+				padding: '6px 7px',
+			},
+			'& .fc-daygrid-day.fc-day-today, & .fc-timegrid-col.fc-day-today': {
+				backgroundColor: '#F1E7DF',
+			},
+			'& .fc-timegrid-divider': {
+				padding: 0,
+				borderColor: colors.dark,
+			},
+			'& .fc-timegrid-slot': {
+				height: 48,
+			},
+			'& .fc-timegrid-slot-label': {
+				verticalAlign: 'top',
+			},
+			'& .fc-timegrid-slot-label-cushion': {
+				...fonts.xxxs,
+				color: colors.gray600,
+				padding: '4px 4px 0 0',
+			},
+			'& .fc-bg-event': {
+				// backgroundColor: colors.white,
+			},
+			'& .fc-timegrid-now-indicator-arrow': {
+				display: 'none',
+			},
+			'& .fc-timegrid-now-indicator-line': {
+				borderColor: colors.primary,
+				'&:before': {
+					top: -4,
+					left: 0,
+					width: 7,
+					height: 7,
+					content: '""',
+					borderRadius: '50%',
+					position: 'absolute',
+					backgroundColor: colors.primary,
+				},
+			},
+			'& .fc-timegrid-event, .fc-timegrid-more-link': {
+				borderRadius: 0,
+			},
+			'& .fc-timegrid-event .fc-event-main': {
+				padding: '7px 11px',
+			},
+			'& .fc-timegrid-event-harness-inset .fc-timegrid-event': {
+				boxShadow: 'none',
+			},
+		},
+	},
+});
