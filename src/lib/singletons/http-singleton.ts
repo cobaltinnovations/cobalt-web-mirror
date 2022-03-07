6c1246e1c6309a573f2178d62b4155b51cebabca
Khaled Shaaban <khaled.shaaban@xmog.com>
2022-03-07 16:36:27 -0500

    temporarily disable FingerprintJS

--- src/lib/singletons/http-singleton.ts
+++ src/lib/singletons/http-singleton.ts
@@ -1,5 +1,5 @@
 import Cookies from 'js-cookie';
-import FingerprintJS from '@fingerprintjs/fingerprintjs-pro';
+// import FingerprintJS from '@fingerprintjs/fingerprintjs-pro';
 
 import { HttpClient } from '@/lib/http-client';
 import config from '@/lib/config';
@@ -15,15 +15,16 @@ export const httpSingleton = new HttpClient({
 	},
 	fingerprintHeaderKey: 'X-Cobalt-Fingerprint-Id',
 	getFingerprintId: () => {
-		const fpConfig: any = { token: config.COBALT_WEB_FINGERPRINTING_TOKEN };
-		if (!!config.COBALT_WEB_FINGERPRINTING_ENDPOINT) {
-			fpConfig.endpoint = config.COBALT_WEB_FINGERPRINTING_ENDPOINT;
-		}
+		return Promise.resolve(undefined);
+		// const fpConfig: any = { token: config.COBALT_WEB_FINGERPRINTING_TOKEN };
+		// if (!!config.COBALT_WEB_FINGERPRINTING_ENDPOINT) {
+		// 	fpConfig.endpoint = config.COBALT_WEB_FINGERPRINTING_ENDPOINT;
+		// }
 
-		return config.COBALT_WEB_FINGERPRINTING_ENABLED?.toLowerCase() === 'true'
-			? FingerprintJS.load(fpConfig)
-					.then((fp) => fp.get())
-					.then((r) => r.visitorId)
-			: Promise.resolve(undefined);
+		// return config.COBALT_WEB_FINGERPRINTING_ENABLED?.toLowerCase() === 'true'
+		// 	? FingerprintJS.load(fpConfig)
+		// 			.then((fp) => fp.get())
+		// 			.then((r) => r.visitorId)
+		// 	: Promise.resolve(undefined);
 	},
 });
