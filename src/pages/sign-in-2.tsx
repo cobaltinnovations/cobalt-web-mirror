c4a82c6be5afe84c6e7616d1fcff3ba016ec7e75
Adam Grayson <asgrayson90@gmail.com>
2022-07-15 10:23:38 -0400

    sso modal

--- src/pages/sign-in-2.tsx
+++ src/pages/sign-in-2.tsx
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import { Container, Row, Col, Button, Form, Modal } from 'react-bootstrap';
 
-import { accountService } from '@/lib/services';
+import { accountService, institutionService } from '@/lib/services';
 import useAccount from '@/hooks/use-account';
 import useHandleError from '@/hooks/use-handle-error';
 import InputHelper from '@/components/input-helper';
 import { createUseThemedStyles } from '@/jss/theme';
 import mediaQueries from '@/jss/media-queries';
+import { AccountSource } from '@/lib/models';
+import useSubdomain from '@/hooks/use-subdomain';
+import useQuery from '@/hooks/use-query';
 
 const useSignInStyles = createUseThemedStyles((theme) => ({
 	signInOuter: {
@@ -26,15 +29,45 @@ const useSignInStyles = createUseThemedStyles((theme) => ({
 }));
 
 const SignIn2 = () => {
+	const subdomain = useSubdomain();
 	const handleError = useHandleError();
 	const { accountSources, subdomainInstitution } = useAccount();
 	const classes = useSignInStyles();
 	const history = useHistory();
 
+	const query = useQuery();
+	const accountSourceId = query.get('accountSourceId');
+
+	const [ssoModalIsOpen, setSsoModalIsOpen] = useState(false);
+	const [ssoOptions, setSsoOptions] = useState<AccountSource[]>([]);
+	const [ssoSelectValue, setSsoSelectValue] = useState<string>('');
 	const [signInForm, setSignInForm] = useState({
 		emailAddress: '',
 		password: '',
 	});
+
+	async function handleSsoModelEnter() {
+		try {
+			const { accountSources } = await institutionService
+				.getAccountSources({
+					...(subdomain && { subdomain }),
+					...(accountSourceId && { accountSourceId }),
+				})
+				.fetch();
+
+			const firstAccountSource = accountSources[0];
+
+			if (accountSources.length === 1) {
+				window.location.href = firstAccountSource.ssoUrl;
+				return;
+			}
+
+			setSsoOptions(accountSources);
+			setSsoSelectValue(firstAccountSource.accountSourceId);
+		} catch (error) {
+			handleError(error);
+		}
+	}
 
 	async function handleSignInFormSubmit(event: React.FormEvent<HTMLFormElement>) {
 		event.preventDefault();
@@ -62,83 +95,144 @@ const SignIn2 = () => {
 	}
 
 	return (
-		<Container fluid className={classes.signInOuter}>
-			<Container className={classes.signIn}>
-				<Row>
-					<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
-						<h3 className="mb-4 text-center">Sign in to Cobalt</h3>
+		<>
+			<Modal
+				show={ssoModalIsOpen}
+				onHide={() => {
+					setSsoModalIsOpen(false);
+				}}
+				onEntering={handleSsoModelEnter}
+				centered
+			>
+				<Modal.Header closeButton>
+					<Modal.Title>Select account type</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<InputHelper
+						label="Sign in with..."
+						value={ssoSelectValue}
+						as="select"
+						onChange={({ currentTarget }) => {
+							setSsoSelectValue(currentTarget.value);
+						}}
+						required
+					>
+						{ssoOptions.map((option) => {
+							return (
+								<option key={option.accountSourceId} value={option.accountSourceId}>
+									{option.description}
+								</option>
+							);
+						})}
+					</InputHelper>
+				</Modal.Body>
+				<Modal.Footer>
+					<div className="text-right">
+						<Button
+							variant="outline-primary"
+							onClick={() => {
+								setSsoModalIsOpen(false);
+							}}
+						>
+							Cancel
+						</Button>
+						<Button
+							className="ms-2"
+							variant="primary"
+							onClick={() => {
+								const option = ssoOptions.find((o) => o.accountSourceId === ssoSelectValue);
 
-						{subdomainInstitution?.ssoEnabled && (
-							<>
-								<div className="mb-6 text-center">
-									<Button
-										className="w-100 d-block"
-										variant="primary"
-										onClick={() => {
-											if (accountSources && accountSources.length === 1) {
-												const firstAccountSource = accountSources[0];
-												window.location.href = firstAccountSource.ssoUrl;
-											} else {
-												history.push('/sign-in-sso');
-											}
-										}}
-									>
-										Continue with SSO
-									</Button>
-								</div>
-								<div className="mb-6 d-flex align-items-center">
-									<hr className="flex-grow-1" />
-									<p className="m-0 px-4 flex-shrink-0">or</p>
-									<hr className="flex-grow-1" />
-								</div>
-							</>
-						)}
+								if (!option) {
+									return;
+								}
 
-						{subdomainInstitution?.emailEnabled && (
-							<>
-								<Form className="mb-6" onSubmit={handleSignInFormSubmit}>
-									<InputHelper
-										className="mb-2"
-										label="Email address"
-										type="email"
-										value={signInForm.emailAddress}
-										onChange={({ currentTarget }) => {
-											setSignInForm((previousValues) => ({
-												...previousValues,
-												emailAddress: currentTarget.value,
-											}));
-										}}
-										required
-									/>
-									<InputHelper
-										className="mb-4"
-										label="Password"
-										type="password"
-										value={signInForm.password}
-										onChange={({ currentTarget }) => {
-											setSignInForm((previousValues) => ({
-												...previousValues,
-												password: currentTarget.value,
-											}));
-										}}
-										required
-									/>
-									<div className="mb-6 text-right">
-										<Link to="/forgot-password">Forgot Password?</Link>
+								window.location.href = option.ssoUrl;
+							}}
+						>
+							Continue
+						</Button>
+					</div>
+				</Modal.Footer>
+			</Modal>
+
+			<Container fluid className={classes.signInOuter}>
+				<Container className={classes.signIn}>
+					<Row>
+						<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+							<h3 className="mb-4 text-center">Sign in to Cobalt</h3>
+
+							{subdomainInstitution?.ssoEnabled && (
+								<>
+									<div className="mb-6 text-center">
+										<Button
+											className="w-100 d-block"
+											variant="primary"
+											onClick={() => {
+												if (accountSources && accountSources.length === 1) {
+													const firstAccountSource = accountSources[0];
+													window.location.href = firstAccountSource.ssoUrl;
+												} else {
+													setSsoModalIsOpen(true);
+												}
+											}}
+										>
+											Continue with SSO
+										</Button>
 									</div>
-									<Button type="submit" className="w-100 d-block" variant="outline-primary">
-										Sign in
-									</Button>
-								</Form>
-								<p className="text-center">
-									Don't have an account? <Link to="/sign-up">Sign up</Link>
-								</p>
-							</>
-						)}
-					</Col>
-				</Row>
+									<div className="mb-6 d-flex align-items-center">
+										<hr className="flex-grow-1" />
+										<p className="m-0 px-4 flex-shrink-0">or</p>
+										<hr className="flex-grow-1" />
+									</div>
+								</>
+							)}
+
+							{subdomainInstitution?.emailEnabled && (
+								<>
+									<Form className="mb-6" onSubmit={handleSignInFormSubmit}>
+										<InputHelper
+											className="mb-2"
+											label="Email address"
+											type="email"
+											value={signInForm.emailAddress}
+											onChange={({ currentTarget }) => {
+												setSignInForm((previousValues) => ({
+													...previousValues,
+													emailAddress: currentTarget.value,
+												}));
+											}}
+											required
+										/>
+										<InputHelper
+											className="mb-4"
+											label="Password"
+											type="password"
+											value={signInForm.password}
+											onChange={({ currentTarget }) => {
+												setSignInForm((previousValues) => ({
+													...previousValues,
+													password: currentTarget.value,
+												}));
+											}}
+											required
+										/>
+										<div className="mb-6 text-right">
+											<Link to="/forgot-password">Forgot Password?</Link>
+										</div>
+										<Button type="submit" className="w-100 d-block" variant="outline-primary">
+											Sign in
+										</Button>
+									</Form>
+									<p className="text-center">
+										Don't have an account? <Link to="/sign-up">Sign up</Link>
+									</p>
+								</>
+							)}
+						</Col>
+					</Row>
+				</Container>
 			</Container>
-		</Container>
+		</>
 	);
 };
 
