0b5e8ec5cd618ee735f5f6308a1199d9ed04f13f
Adam Grayson <asgrayson90@gmail.com>
2022-09-26 13:57:28 -0400

    restyle and update covid-19 resources

--- src/pages/covid-19-resources.tsx
+++ src/pages/covid-19-resources.tsx
@@ -1,21 +1,34 @@
 import React, { FC } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 
-import useAccount from '@/hooks/use-account';
 import HeroContainer from '@/components/hero-container';
 
-const Covid19Resources: FC = () => {
-	const { institution } = useAccount();
+import { COVID_19_RESOURCES } from '@/covid-19-resources';
 
+const Covid19Resources: FC = () => {
 	return (
 		<>
 			<HeroContainer>
 				<h2 className="mb-0 text-center">Covid-19 Resources</h2>
 			</HeroContainer>
-			<Container className="pt-5 pb-5">
+			<Container className="py-10 py-lg-14">
 				<Row>
 					<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
-						<div dangerouslySetInnerHTML={{ __html: institution?.covidContent ?? '' }} />
+						{COVID_19_RESOURCES.map((resource, index) => {
+							const isLast = COVID_19_RESOURCES.length - 1 === index;
+
+							return (
+								<React.Fragment key={index}>
+									<h4 className="mb-2">
+										<a href={resource.href} target="_blank" rel="noreferrer">
+											{resource.title}
+										</a>
+									</h4>
+									<p>{resource.description}</p>
+									{!isLast && <hr className="my-8" />}
+								</React.Fragment>
+							);
+						})}
 					</Col>
 				</Row>
 			</Container>
