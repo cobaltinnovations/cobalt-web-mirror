d6c6c819ff51a3c7ce654d1fef4068ff64fa94b7
Adam Grayson <asgrayson90@gmail.com>
2022-09-29 14:34:41 -0400

    contact us guard for institution

--- src/lib/models/institution.ts
+++ src/lib/models/institution.ts
@@ -1,21 +1,22 @@
 export interface Institution {
 	anonymousEnabled: boolean;
+	contactUsEnabled: boolean;
 	calendarDescription: string;
+	contentScreeningFlowId?: string;
 	covidContent: string;
 	crisisContent: string;
 	emailEnabled: boolean;
-	immediateAccessEnabled: boolean;
 	emailSignupEnabled: boolean;
+	groupSessionsScreeningFlowId?: string;
+	immediateAccessEnabled: boolean;
 	institutionId: string;
 	privacyContent: string;
-	contentScreeningFlowId?: string;
-	groupSessionsScreeningFlowId?: string;
 	providerTriageScreeningFlowId: string;
 	requireConsentForm: boolean;
 	ssoEnabled: boolean;
+	supportEmailAddress: string;
 	supportEnabled: boolean;
 	wellBeingContent: string;
-	supportEmailAddress: string;
 }
 
 export interface AccountSource {
