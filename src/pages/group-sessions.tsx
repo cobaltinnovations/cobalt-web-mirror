129215234362f638b0132ffc72452ae7680c4214
Adam Grayson <asgrayson90@gmail.com>
2023-01-19 15:10:17 -0500

    loading states are better

--- src/pages/group-sessions.tsx
+++ src/pages/group-sessions.tsx
@@ -12,8 +12,10 @@ import Loader from '@/components/loader';
 import HeroContainer from '@/components/hero-container';
 import InputHelperSearch from '@/components/input-helper-search';
 import StudioEvent, { StudioEventSkeleton } from '@/components/studio-event';
+import useHandleError from '@/hooks/use-handle-error';
 
 const GroupSessions = () => {
+	const handleError = useHandleError();
 	const { mixpanel } = useAnalytics();
 	const navigate = useNavigate();
 
@@ -27,6 +29,8 @@ const GroupSessions = () => {
 	const searchInputRef = useRef<HTMLInputElement>(null);
 	const [searchInputValue, setSearchInputValue] = useState(groupSessionSearchQuery);
 
+	const [isLoading, setIsLoading] = useState(false);
+	const [isFirstLoad, setIsFirstLoad] = useState(true);
 	const [groupSessions, setGroupSessions] = useState<GroupSessionModel[]>([]);
 	const [groupSessionsTotalCount, setGroupSessionsTotalCount] = useState(0);
 	const { institution } = useAccount();
@@ -45,23 +49,34 @@ const GroupSessions = () => {
 	}, [didCheckScreeningSessions, hasTouchScreen]);
 
 	const fetchData = useCallback(async () => {
-		console.log(pageNumber.current);
+		try {
+			setIsLoading(true);
 
-		const { groupSessions, totalCount } = await groupSessionsService
-			.getGroupSessions({
-				viewType: 'PATIENT',
-				groupSessionStatusId: GROUP_SESSION_STATUS_ID.ADDED,
-				orderBy: GROUP_SESSION_SORT_ORDER.START_TIME_ASCENDING,
-				urlName: groupSessionUrlName,
-				searchQuery: groupSessionSearchQuery,
-				pageSize: pageSize.current,
-				pageNumber: pageNumber.current,
-			})
-			.fetch();
+			const { groupSessions, totalCount } = await groupSessionsService
+				.getGroupSessions({
+					viewType: 'PATIENT',
+					groupSessionStatusId: GROUP_SESSION_STATUS_ID.ADDED,
+					orderBy: GROUP_SESSION_SORT_ORDER.START_TIME_ASCENDING,
+					urlName: groupSessionUrlName,
+					searchQuery: groupSessionSearchQuery,
+					pageSize: pageSize.current,
+					pageNumber: pageNumber.current,
+				})
+				.fetch();
 
-		setGroupSessions((previousValue) => previousValue.concat(groupSessions));
-		setGroupSessionsTotalCount(totalCount);
-	}, [groupSessionUrlName, groupSessionSearchQuery]);
+			setGroupSessions((previousValue) => previousValue.concat(groupSessions));
+			setGroupSessionsTotalCount(totalCount);
+		} catch (error) {
+			handleError(error);
+		} finally {
+			setIsLoading(false);
+			setIsFirstLoad(false);
+		}
+	}, [groupSessionSearchQuery, groupSessionUrlName, handleError]);
+
+	useEffect(() => {
+		fetchData();
+	}, [fetchData]);
 
 	const disableViewMore = groupSessionsTotalCount <= pageNumber.current + 1 * pageSize.current;
 	const handleViewMoreButtonClick = useCallback(() => {
@@ -150,107 +165,110 @@ const GroupSessions = () => {
 				</Form>
 			</HeroContainer>
 
-			<AsyncPage
-				fetchData={fetchData}
-				loadingComponent={
-					<Container className="py-10">
-						<Row className="mb-10">
-							<Col>
-								<h2>Upcoming Sessions</h2>
-							</Col>
-						</Row>
-						<Row>
-							<Col md={6} lg={4} className="mb-8">
-								<StudioEventSkeleton />
-							</Col>
-							<Col md={6} lg={4} className="mb-8">
-								<StudioEventSkeleton />
-							</Col>
-							<Col md={6} lg={4} className="mb-8">
-								<StudioEventSkeleton />
-							</Col>
-							<Col md={6} lg={4} className="mb-8">
-								<StudioEventSkeleton />
-							</Col>
-							<Col md={6} lg={4} className="mb-8">
-								<StudioEventSkeleton />
-							</Col>
-							<Col md={6} lg={4} className="mb-8">
-								<StudioEventSkeleton />
-							</Col>
-						</Row>
-					</Container>
-				}
-			>
-				<Container className="py-10">
-					<Row className="mb-10">
+			<Container className="py-10">
+				<Row className="mb-10">
+					<Col>
+						<h2>Upcoming Sessions</h2>
+					</Col>
+				</Row>
+				{!isLoading && groupSessions.length <= 0 && (
+					<Row className="mb-2">
 						<Col>
-							<h2>Upcoming Sessions</h2>
+							<p className="text-center mb-0">
+								{groupSessionSearchQuery ? (
+									'There are no matching results.'
+								) : groupSessionUrlName ? (
+									<>
+										There are no group sessions available for the selected type.
+										<Button
+											size="sm"
+											variant="link"
+											onClick={() => {
+												searchParams.delete('class');
+												setSearchParams(searchParams, { replace: true });
+											}}
+										>
+											Click here to view all available group sessions.
+										</Button>
+									</>
+								) : (
+									'There are no group sessions available.'
+								)}
+							</p>
 						</Col>
 					</Row>
+				)}
+				{groupSessions.length > 0 && (
 					<Row className="mb-2">
-						{groupSessions.length > 0 ? (
-							groupSessions.map((groupSession) => {
-								return (
-									<Col md={6} lg={4} key={groupSession.groupSessionId} className="mb-8">
-										<Link
-											className="d-block text-decoration-none h-100"
-											to={`/in-the-studio/group-session-scheduled/${groupSession.groupSessionId}`}
-										>
-											<StudioEvent className="h-100" studioEvent={groupSession} />
-										</Link>
-									</Col>
-								);
-							})
-						) : (
+						{groupSessions.map((groupSession) => {
+							return (
+								<Col md={6} lg={4} key={groupSession.groupSessionId} className="mb-8">
+									<Link
+										className="d-block text-decoration-none h-100"
+										to={`/in-the-studio/group-session-scheduled/${groupSession.groupSessionId}`}
+									>
+										<StudioEvent className="h-100" studioEvent={groupSession} />
+									</Link>
+								</Col>
+							);
+						})}
+					</Row>
+				)}
+				{isLoading && (
+					<Row className="mb-10">
+						{isFirstLoad ? (
 							<>
-								<p className="text-center mb-0">
-									{groupSessionSearchQuery ? (
-										'There are no matching results.'
-									) : groupSessionUrlName ? (
-										<>
-											There are no group sessions available for the selected type.
-											<Button
-												size="sm"
-												variant="link"
-												onClick={() => {
-													searchParams.delete('class');
-													setSearchParams(searchParams, { replace: true });
-												}}
-											>
-												Click here to view all available group sessions.
-											</Button>
-										</>
-									) : (
-										'There are no group sessions available.'
-									)}
-								</p>
+								<Col md={6} lg={4} className="mb-8">
+									<StudioEventSkeleton />
+								</Col>
+								<Col md={6} lg={4} className="mb-8">
+									<StudioEventSkeleton />
+								</Col>
+								<Col md={6} lg={4} className="mb-8">
+									<StudioEventSkeleton />
+								</Col>
+								<Col md={6} lg={4} className="mb-8">
+									<StudioEventSkeleton />
+								</Col>
+								<Col md={6} lg={4} className="mb-8">
+									<StudioEventSkeleton />
+								</Col>
+								<Col md={6} lg={4} className="mb-8">
+									<StudioEventSkeleton />
+								</Col>
 							</>
+						) : (
+							<Col>
+								<Loader className="position-relative" />
+							</Col>
 						)}
 					</Row>
-					<Row>
-						<Col></Col>
-						<Col>
-							{!disableViewMore && (
-								<div className="text-center">
-									<Button onClick={handleViewMoreButtonClick}>View More</Button>
-								</div>
+				)}
+				<Row>
+					<Col></Col>
+					<Col>
+						{!disableViewMore && (
+							<div className="text-center">
+								<Button onClick={handleViewMoreButtonClick} disabled={isLoading}>
+									View More
+								</Button>
+							</div>
+						)}
+					</Col>
+					<Col>
+						<div className="text-right">
+							{institution?.userSubmittedGroupSessionEnabled && (
+								<Button
+									variant="link"
+									onClick={() => {
+										mixpanel.track('Patient-Sourced Add Group Session Click', {});
+										navigate('/group-sessions/scheduled/create');
+									}}
+								>
+									Submit a Session
+								</Button>
 							)}
-						</Col>
-						<Col>
-							<div className="text-right">
-								{institution?.userSubmittedGroupSessionEnabled && (
-									<Button
-										variant="link"
-										onClick={() => {
-											mixpanel.track('Patient-Sourced Add Group Session Click', {});
-											navigate('/group-sessions/scheduled/create');
-										}}
-									>
-										Submit a Session
-									</Button>
-								)}
-								{/* {institution?.userSubmittedGroupSessionRequestEnabled && (
+							{/* {institution?.userSubmittedGroupSessionRequestEnabled && (
 									<Button
 										variant="link"
 										onClick={() => {
@@ -261,28 +279,27 @@ const GroupSessions = () => {
 										Submit a Session
 									</Button>
 								)} */}
-							</div>
-						</Col>
-					</Row>
-				</Container>
-				<HeroContainer className="bg-n75">
-					<h2 className="mb-6 text-center">Looking to schedule a group session for your team?</h2>
-					<p className="mb-6 fs-large text-center">
-						Request a session and we'll work with you to find a dedicated time for a wellness-focused group
-						session for your team.
-					</p>
-					<div className="text-center">
-						<Button
-							variant="outline-primary"
-							onClick={() => {
-								navigate('/group-sessions/request');
-							}}
-						>
-							Request a Session
-						</Button>
-					</div>
-				</HeroContainer>
-			</AsyncPage>
+						</div>
+					</Col>
+				</Row>
+			</Container>
+			<HeroContainer className="bg-n75">
+				<h2 className="mb-6 text-center">Looking to schedule a group session for your team?</h2>
+				<p className="mb-6 fs-large text-center">
+					Request a session and we'll work with you to find a dedicated time for a wellness-focused group
+					session for your team.
+				</p>
+				<div className="text-center">
+					<Button
+						variant="outline-primary"
+						onClick={() => {
+							navigate('/group-sessions/request');
+						}}
+					>
+						Request a Session
+					</Button>
+				</div>
+			</HeroContainer>
 		</>
 	);
 };
