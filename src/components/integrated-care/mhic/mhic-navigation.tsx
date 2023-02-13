641eea059813bfafc9f0a6cef66b565d73965fd5
Adam Grayson <asgrayson90@gmail.com>
2023-02-13 15:04:00 -0500

    navigation bubbles and active state

--- src/components/integrated-care/mhic/mhic-navigation.tsx
+++ src/components/integrated-care/mhic/mhic-navigation.tsx
@@ -1,5 +1,5 @@
-import React, { useRef } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useMemo, useRef } from 'react';
+import { Link, useLocation, useSearchParams } from 'react-router-dom';
 
 import { buildQueryParamUrl } from '@/lib/utils';
 import { createUseThemedStyles } from '@/jss/theme';
@@ -13,63 +13,110 @@ const useStyles = createUseThemedStyles((theme) => ({
 		borderBottom: `1px solid ${theme.colors.n100}`,
 		'& a': {
 			padding: '18px 10px',
+			position: 'relative',
+			alignItems: 'center',
+			display: 'inline-flex',
 			textDecoration: 'none',
 			color: theme.colors.n500,
 			...theme.fonts.bodyNormal,
+			'&:after': {
+				left: 10,
+				right: 10,
+				bottom: 0,
+				height: 2,
+				content: '""',
+				display: 'none',
+				position: 'absolute',
+				backgroundColor: theme.colors.p500,
+			},
 			'&:hover': {
 				color: theme.colors.p500,
 			},
 			'&:first-of-type': {
 				paddingLeft: 0,
+				'&:after': {
+					left: 0,
+				},
 			},
 			'&:last-of-type': {
 				paddingRight: 0,
+				'&:after': {
+					right: 0,
+				},
 			},
 			'&.first-link-of-section': {
 				paddingLeft: 20,
+				'&:after': {
+					left: 20,
+				},
 			},
 			'&.last-link-of-section': {
 				paddingRight: 20,
 				borderRight: `1px solid ${theme.colors.n100}`,
+				'&:after': {
+					right: 20,
+				},
+			},
+			'&.active': {
+				color: theme.colors.p500,
+				'&:after': {
+					display: 'block',
+				},
 			},
 		},
+	},
+	countBubble: {
+		marginLeft: 4,
+		padding: '0 8px',
+		borderRadius: 32,
+		...theme.fonts.default,
+		color: theme.colors.p500,
+		...theme.fonts.headingBold,
+		backgroundColor: theme.colors.p50,
 	},
 }));
 
 export const MhicNavigation = () => {
-	const { pathname } = useLocation();
 	const classes = useStyles();
+	const { pathname } = useLocation();
+	const [searchParams] = useSearchParams();
+	const statusParam = useMemo(() => searchParams.get('status'), [searchParams]);
 
 	const linkSections = useRef([
 		{
 			links: [
 				{
-					to: buildQueryParamUrl(pathname, { status: 'NEED_ASSESSMENT' }),
+					id: 'NEED_ASSESSMENT',
 					title: 'Need Assessment',
+					count: 4,
 				},
 			],
 		},
 		{
 			links: [
 				{
-					to: buildQueryParamUrl(pathname, { status: 'SAFETY_PLANNING' }),
+					id: 'SAFETY_PLANNING',
 					title: 'Safety Planning',
+					count: 2,
 				},
 				{
-					to: buildQueryParamUrl(pathname, { status: 'SPECIALTY_CARE' }),
+					id: 'SPECIALTY_CARE',
 					title: 'Specialty Care',
+					count: 4,
 				},
 				{
-					to: buildQueryParamUrl(pathname, { status: 'BEHAVIORAL_HEALTH_PROVIDER' }),
+					id: 'BEHAVIORAL_HEALTH_PROVIDER',
 					title: 'Behavioral Health Provider',
+					count: 0,
 				},
 			],
 		},
 		{
 			links: [
 				{
-					to: buildQueryParamUrl(pathname, { status: 'CLOSED' }),
+					id: 'CLOSED',
 					title: 'Closed',
+					count: 0,
 				},
 			],
 		},
@@ -90,13 +137,15 @@ export const MhicNavigation = () => {
 							return (
 								<Link
 									key={linkIndex}
-									to={link.to}
+									to={buildQueryParamUrl(pathname, { status: link.id })}
 									className={classNames({
 										'first-link-of-section': isFirstLink && !isFirstSection,
 										'last-link-of-section': isLastLink && !isLastSection,
+										active: statusParam === link.id,
 									})}
 								>
-									{link.title}
+									<span>{link.title}</span>
+									{link.count > 0 && <span className={classes.countBubble}>{link.count}</span>}
 								</Link>
 							);
 						})}
