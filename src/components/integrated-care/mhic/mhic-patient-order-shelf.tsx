2a89ba27e5e50b0c5142b43c1ccbd73669e16d7e
Adam Grayson <asgrayson90@gmail.com>
2023-02-14 15:41:58 -0500

    mhic shelf header

--- src/components/integrated-care/mhic/mhic-patient-order-shelf.tsx
+++ src/components/integrated-care/mhic/mhic-patient-order-shelf.tsx
@@ -1,6 +1,12 @@
 import { createUseThemedStyles } from '@/jss/theme';
 import React from 'react';
+import { Badge, Button } from 'react-bootstrap';
 import { CSSTransition } from 'react-transition-group';
+import CopyToClipboard from 'react-copy-to-clipboard';
+
+import { ReactComponent as CloseIcon } from '@/assets/icons/icon-close.svg';
+import { ReactComponent as CopyIcon } from '@/assets/icons/icon-content-copy.svg';
+import useFlags from '@/hooks/use-flags';
 
 const useStyles = createUseThemedStyles((theme) => ({
 	patientOrderShelf: {
@@ -8,12 +14,12 @@ const useStyles = createUseThemedStyles((theme) => ({
 		right: 0,
 		bottom: 0,
 		zIndex: 6,
-		width: '100%',
-		maxWidth: 480,
+		width: '95%',
+		maxWidth: 800,
 		position: 'fixed',
 		overflow: 'hidden',
 		boxShadow: theme.elevation.e400,
-		backgroundColor: theme.colors.n0,
+		backgroundColor: theme.colors.n50,
 	},
 	overlay: {
 		top: 0,
@@ -27,6 +33,15 @@ const useStyles = createUseThemedStyles((theme) => ({
 	},
 	header: {
 		padding: '28px 32px',
+		position: 'relative',
+		backgroundColor: theme.colors.n0,
+		borderBottom: `1px solid ${theme.colors.n100}`,
+	},
+	shelfCloseButton: {
+		top: 20,
+		right: 24,
+		padding: 8,
+		position: 'absolute',
 	},
 	'@global': {
 		'.patient-order-shelf-enter': {
@@ -71,14 +86,42 @@ interface MhicPatientOrderShelfProps {
 
 export const MhicPatientOrderShelf = ({ open, onHide }: MhicPatientOrderShelfProps) => {
 	const classes = useStyles();
+	const { addFlag } = useFlags();
 
 	return (
 		<>
 			<CSSTransition in={open} timeout={300} classNames="patient-order-shelf" mountOnEnter unmountOnExit>
 				<div className={classes.patientOrderShelf}>
 					<div className={classes.header}>
-						<h4>Customize Views</h4>
-						<p>[TODO]: Content of this shelf</p>
+						<Button variant="link" className={classes.shelfCloseButton} onClick={onHide}>
+							<CloseIcon />
+						</Button>
+						<div className="mb-2 d-flex align-items-center">
+							<h4 className="mb-0 me-2">Lastname, FirstName</h4>
+							<Badge pill bg="outline-primary">
+								NEW
+							</Badge>
+						</div>
+						<div className="d-flex align-items-center">
+							<p className="mb-0">
+								MRN: <span className="fw-bold">1A2B3C4D5E</span>
+							</p>
+							<CopyToClipboard
+								onCopy={() => {
+									addFlag({
+										variant: 'success',
+										title: 'Copied!',
+										description: 'The MRN for this patient was copied to your clipboard',
+										actions: [],
+									});
+								}}
+								text="1A2B3C4D5E"
+							>
+								<Button variant="link" className="p-2">
+									<CopyIcon width={20} height={20} />
+								</Button>
+							</CopyToClipboard>
+						</div>
 					</div>
 				</div>
 			</CSSTransition>
