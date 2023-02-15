081d40f764d6152c89e4bb1a2a6141aa75421eaf
Adam Grayson <asgrayson90@gmail.com>
2023-02-15 11:06:09 -0500

    make patient details its own component

---
+++ src/components/integrated-care/mhic/mhic-patient-details.tsx
@@ -0,0 +1,413 @@
+import React from 'react';
+import { Badge, Button, Card, Col, Container, Row } from 'react-bootstrap';
+
+export const MhicPatientDetails = () => {
+	return (
+		<>
+			<section>
+				<Container fluid>
+					<Row className="mb-6">
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Basic Info</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row className="mb-4">
+											<Col>
+												<p className="m-0">Firstname Lastname</p>
+											</Col>
+										</Row>
+										<Row className="mb-4">
+											<Col>
+												<p className="m-0 text-gray">Date of birth</p>
+											</Col>
+											<Col>
+												<p className="m-0">3/22/1953</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<p className="m-0 text-gray">Pref. Language</p>
+											</Col>
+											<Col>
+												<p className="m-0">English</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Demographics</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row className="mb-4">
+											<Col>
+												<p className="m-0 text-gray">Race</p>
+											</Col>
+											<Col>
+												<p className="m-0">Black</p>
+											</Col>
+										</Row>
+										<Row className="mb-4">
+											<Col>
+												<p className="m-0 text-gray">Ethnicity</p>
+											</Col>
+											<Col>
+												<p className="m-0">Hispanic</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<p className="m-0 text-gray">Gender Identity</p>
+											</Col>
+											<Col>
+												<p className="m-0">Male</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Address</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row>
+											<Col>
+												<p className="m-0">123 Main Street</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<p className="m-0">Apt 21</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<p className="m-0">Philadelphia, PA 19238</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Insurance</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row>
+											<Col>
+												<p className="m-0">Medicare</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<p className="m-0">Plan: [PLAN_NAME]</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<p className="m-0">Coverage Until: Mar 23, 2023</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+			<section>
+				<Container fluid>
+					<Row className="mb-6">
+						<Col>
+							<h4 className="mb-0">Contact</h4>
+						</Col>
+					</Row>
+					<Row className="mb-6">
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Patient</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row className="mb-4">
+											<Col xs={3}>
+												<p className="m-0 text-gray">Home Phone</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">(000) 000-0000</p>
+											</Col>
+										</Row>
+										<Row className="mb-4">
+											<Col xs={3}>
+												<p className="m-0 text-gray">Mobile Phone</p>
+											</Col>
+											<Col xs={9}>
+												<div className="d-flex align-items-center">
+													<p className="m-0">(000) 000-0000</p>
+													<Badge bg="outline-dark" pill className="ms-2">
+														Preferred
+													</Badge>
+												</div>
+											</Col>
+										</Row>
+										<Row>
+											<Col xs={3}>
+												<p className="m-0 text-gray">Email</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">frankasanzez@email.com</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Patient's Father</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row>
+											<Col xs={3}>
+												<p className="m-0 text-gray">Mobile Phone</p>
+											</Col>
+											<Col xs={9}>
+												<div className="d-flex align-items-center">
+													<p className="m-0 text-gray">(000) 000-0000</p>
+													<Badge bg="outline-dark" pill className="ms-2">
+														Preferred
+													</Badge>
+												</div>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+			<section>
+				<Container fluid>
+					<Row className="mb-6">
+						<Col>
+							<h4 className="mb-0">Referrals</h4>
+						</Col>
+					</Row>
+					<Row className="mb-6">
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Most Recent</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row>
+											<Col>
+												<div className="mb-1 d-flex align-items-center justify-content-between">
+													<p className="m-0 fw-bold">Nov 12, 2022 (Episode: 6 days)</p>
+													<div className="d-flex align-items-center">
+														<p className="m-0 fw-bold text-success">Active</p>
+														<Button variant="primary" size="sm" className="ms-4">
+															Close Episode
+														</Button>
+													</div>
+												</div>
+												<p className="mb-1 text-gray">Referred by: [PRACTICE_NAME]</p>
+												<p className="m-0 text-gray">
+													High PHQ-4 score, patient reported panic attacks
+												</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Past</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row className="mb-4 pb-6 border-bottom">
+											<Col>
+												<div className="mb-1 d-flex align-items-center justify-content-between">
+													<p className="m-0 fw-bold">Nov 12, 2022 (Episode: 6 days)</p>
+													<div className="d-flex align-items-center">
+														<p className="m-0 fw-bold text-gray">Closed</p>
+														<Button variant="primary" size="sm" className="ms-4" disabled>
+															Reopen
+														</Button>
+													</div>
+												</div>
+												<p className="mb-1 text-gray">Referred by: [PRACTICE_NAME]</p>
+												<p className="m-0 text-gray">
+													High PHQ-4 score, patient reported panic attacks
+												</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col>
+												<div className="mb-1 d-flex align-items-center justify-content-between">
+													<p className="m-0 fw-bold">Nov 12, 2022 (Episode: 6 days)</p>
+													<div className="d-flex align-items-center">
+														<p className="m-0 fw-bold text-gray">Closed</p>
+														<Button variant="primary" size="sm" className="ms-4" disabled>
+															Reopen
+														</Button>
+													</div>
+												</div>
+												<p className="mb-1 text-gray">Referred by: [PRACTICE_NAME]</p>
+												<p className="m-0 text-gray">
+													High PHQ-4 score, patient reported panic attacks
+												</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+			<section>
+				<Container fluid>
+					<Row className="mb-6">
+						<Col>
+							<h4 className="mb-0">Clinical</h4>
+						</Col>
+					</Row>
+					<Row className="mb-6">
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Diagnoses</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row>
+											<Col>
+												<p className="m-0">[VALUE_WITH_F_CODE]</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+					<Row className="mb-6">
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Medications</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row>
+											<Col>
+												<p className="m-0">Psychotherapeutic Med Lst 2 Weeks</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Card bsPrefix="ic-card">
+								<Card.Header>
+									<Card.Title>Care Team</Card.Title>
+								</Card.Header>
+								<Card.Body>
+									<Container fluid>
+										<Row className="mb-4">
+											<Col xs={3}>
+												<p className="m-0 text-gray">Practice</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">Practice Name</p>
+											</Col>
+										</Row>
+										<Row className="mb-4">
+											<Col xs={3}>
+												<p className="m-0 text-gray">Ordering Provider</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">Theresa V Rollins, MD</p>
+											</Col>
+										</Row>
+										<Row className="mb-4">
+											<Col xs={3}>
+												<p className="m-0 text-gray">Billing Provider</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">James L Wong, MD</p>
+											</Col>
+										</Row>
+										<Row className="mb-4">
+											<Col xs={3}>
+												<p className="m-0 text-gray">PC Provider</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">James L Wong, MD</p>
+											</Col>
+										</Row>
+										<Row className="mb-4">
+											<Col xs={3}>
+												<p className="m-0 text-gray">MHIC</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">Ava Williams</p>
+											</Col>
+										</Row>
+										<Row className="mb-4">
+											<Col xs={3}>
+												<p className="m-0 text-gray">BHP</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">Sally Benson</p>
+											</Col>
+										</Row>
+										<Row>
+											<Col xs={3}>
+												<p className="m-0 text-gray">Psychiatrist</p>
+											</Col>
+											<Col xs={9}>
+												<p className="m-0">Ursula Forrester</p>
+											</Col>
+										</Row>
+									</Container>
+								</Card.Body>
+							</Card>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		</>
+	);
+};
