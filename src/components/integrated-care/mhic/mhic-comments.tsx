7dcf5be35bead331fd526dd7b8dc9b272c37a996
Adam Grayson <asgrayson90@gmail.com>
2023-02-21 13:21:33 -0500

    remove lodash clonedeep

--- src/components/integrated-care/mhic/mhic-comments.tsx
+++ src/components/integrated-care/mhic/mhic-comments.tsx
@@ -1,4 +1,3 @@
-import { cloneDeep } from 'lodash';
 import React, { useCallback, useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import classNames from 'classnames';
