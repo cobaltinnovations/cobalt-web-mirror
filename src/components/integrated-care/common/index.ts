5e712e4038cd3afcebe61167024636fbfd885089
Adam Grayson <asgrayson90@gmail.com>
2023-05-22 13:27:15 -0400

    swap new template in for old routes

--- src/components/integrated-care/common/index.ts
+++ src/components/integrated-care/common/index.ts
@@ -1,4 +1,1 @@
-export * from './patient-address-form-inputs';
-export * from './patient-demographics-form-inputs';
-export * from './patient-details-form-inputs';
 export * from './screening-intro';
--- src/routes/ic/patient/patient-demographics.tsx
+++ src/routes/ic/patient/patient-demographics.tsx
@@ -1,74 +1,109 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
-import { Formik } from 'formik';
 
+import { PatientOrderCarePreferenceId, PatientOrderModel } from '@/lib/models';
 import { integratedCareService } from '@/lib/services';
 import useHandleError from '@/hooks/use-handle-error';
-import {
-	PatientAddressFormData,
-	PatientAddressFormInputs,
-	PatientDemographicsFormData,
-	PatientDemographicsFormInputs,
-	PatientDetailsFormData,
-	PatientDetailsFormInputs,
-} from '@/components/integrated-care/common';
 import { useIntegratedCareLoaderData } from '../landing';
-import { PatientOrderModel } from '@/lib/models';
 import AsyncWrapper from '@/components/async-page';
+import InputHelper from '@/components/input-helper';
 
-type PatientInfoFormData = PatientDetailsFormData & PatientAddressFormData & PatientDemographicsFormData;
+const carePreferences = [
+	{
+		patientOrderCarePreferenceId: PatientOrderCarePreferenceId.NO_PREFERENCE,
+		title: 'No Preference',
+	},
+	{
+		patientOrderCarePreferenceId: PatientOrderCarePreferenceId.TELEHEALTH,
+		title: 'Telehealth',
+	},
+	{
+		patientOrderCarePreferenceId: PatientOrderCarePreferenceId.IN_PERSON,
+		title: 'In-persion',
+	},
+];
+
+const careRadii = [
+	{
+		inPersonCareRadiusId: 'TEN_MILES',
+		inPersonCareRadius: 10,
+		title: '10 miles',
+	},
+	{
+		inPersonCareRadiusId: 'TWENTY_MILES',
+		inPersonCareRadius: 20,
+		title: '20 miles',
+	},
+	{
+		inPersonCareRadiusId: 'THIRTY_MILES',
+		inPersonCareRadius: 30,
+		title: '30 miles',
+	},
+	{
+		inPersonCareRadiusId: 'FORTY_FIVE_MILES',
+		inPersonCareRadius: 45,
+		title: '45 miles',
+	},
+	{
+		inPersonCareRadiusId: 'SIXTY_MILES',
+		inPersonCareRadius: 60,
+		title: '60 miles',
+	},
+];
 
 const PatientDemographics = () => {
-	const { referenceDataResponse } = useIntegratedCareLoaderData();
 	const navigate = useNavigate();
 	const handleError = useHandleError();
-	const [isSaving, setIsSaving] = useState(false);
+	const { referenceDataResponse } = useIntegratedCareLoaderData();
 	const [patientOrder, setPatientOrder] = useState<PatientOrderModel>();
-
-	const initialFormValues: PatientInfoFormData = useMemo(() => {
-		return {
-			patientFirstName: patientOrder?.patientFirstName ?? '',
-			patientLastName: patientOrder?.patientLastName ?? '',
-			patientBirthdate: patientOrder?.patientBirthdate ?? '',
-			patientPhoneNumber: patientOrder?.patientPhoneNumberDescription ?? '',
-			patientEmailAddress: patientOrder?.patientEmailAddress ?? patientOrder?.patientAccount?.emailAddress ?? '',
-			patientOrderInsurancePayorId: patientOrder?.patientOrderInsurancePayorId ?? '',
-			patientOrderInsurancePlanId: patientOrder?.patientOrderInsurancePlanId ?? '',
-			patientAddress: {
-				streetAddress1: patientOrder?.patientAddress?.streetAddress1 ?? '',
-				streetAddress2: patientOrder?.patientAddress?.streetAddress2 ?? '',
-				locality: patientOrder?.patientAddress?.locality ?? '',
-				region: patientOrder?.patientAddress?.region ?? '',
-				postalCode: patientOrder?.patientAddress?.postalCode ?? '',
-				postalName: `${patientOrder?.patientFirstName} ${patientOrder?.patientLastName}`,
-				countryCode: 'US',
-			},
-			patientBirthSexId: patientOrder?.patientBirthSexId ?? '',
-			patientGenderIdentityId: patientOrder?.patientGenderIdentityId ?? '',
-			patientRaceId: patientOrder?.patientRaceId ?? '',
-			patientEthnicityId: patientOrder?.patientEthnicityId ?? '',
-			patientLanguageCode: patientOrder?.patientLanguageCode ?? '',
-		};
-	}, [patientOrder]);
+	const [formValues, setFormValues] = useState({
+		patientPhoneNumber: '',
+		patientEmailAddress: '',
+		patientOrderCarePreferenceId: '',
+		inPersonCareRadius: 0,
+		patientLanguageCode: '',
+		patientRaceId: '',
+		patientEthnicityId: '',
+		patientBirthSexId: '',
+		patientGenderIdentityId: '',
+		verified: false,
+	});
+	const [isSaving, setIsSaving] = useState(false);
 
 	const fetchData = useCallback(async () => {
 		const response = await integratedCareService.getLatestPatientOrder().fetch();
+
 		setPatientOrder(response.patientOrder);
+		setFormValues({
+			patientPhoneNumber: response.patientOrder.patientPhoneNumberDescription ?? '',
+			patientEmailAddress:
+				response.patientOrder.patientEmailAddress ?? response.patientOrder.patientAccount?.emailAddress ?? '',
+			patientOrderCarePreferenceId: response.patientOrder.patientOrderCarePreferenceId ?? '',
+			inPersonCareRadius: response.patientOrder.inPersonCareRadius ?? 0,
+			patientLanguageCode: response.patientOrder.patientLanguageCode ?? '',
+			patientRaceId: response.patientOrder.patientRaceId ?? '',
+			patientEthnicityId: response.patientOrder.patientEthnicityId ?? '',
+			patientBirthSexId: response.patientOrder.patientBirthSexId ?? '',
+			patientGenderIdentityId: response.patientOrder.patientGenderIdentityId ?? '',
+			verified: false,
+		});
 	}, []);
 
-	const handleFormikSubmit = useCallback(
-		async (values: PatientInfoFormData) => {
-			if (!patientOrder) {
-				return;
-			}
+	const handleFormSubmit = useCallback(
+		async (event: React.FormEvent<HTMLFormElement>) => {
+			event.preventDefault();
 
 			try {
 				setIsSaving(true);
 
+				if (!patientOrder) {
+					throw new Error('patientOrder is undefined.');
+				}
+
 				await integratedCareService
 					.patchPatientOrder(patientOrder.patientOrderId, {
-						...values,
+						...formValues,
 						patientDemographicsConfirmed: true,
 					})
 					.fetch();
@@ -79,61 +114,264 @@ const PatientDemographics = () => {
 				handleError(error);
 			}
 		},
-		[handleError, navigate, patientOrder]
+		[formValues, handleError, navigate, patientOrder]
 	);
 
 	return (
 		<AsyncWrapper fetchData={fetchData}>
 			<Container className="py-20">
-				<Row className="mb-8">
+				<Row className="mb-6">
 					<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
 						<h3 className="mb-2">Verify your information</h3>
-						<p className="mb-0">
+						<p className="mb-8">
 							Your primary care team gave us a head start filling out this information. Please make sure
 							all information is correct and complete before continuing.
 						</p>
+						<hr />
 					</Col>
 				</Row>
-				<Row className="mb-8">
-					<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
-						<Formik<PatientInfoFormData>
-							initialValues={initialFormValues}
-							enableReinitialize
-							onSubmit={handleFormikSubmit}
-						>
-							{(formikProps) => (
-								<Form onSubmit={formikProps.handleSubmit}>
-									<PatientDetailsFormInputs
-										//@ts-expect-error
-										formikProps={formikProps}
+				<Form onSubmit={handleFormSubmit}>
+					<Row className="mb-6">
+						<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+							<h4 className="mb-6">Contact Information</h4>
+							<InputHelper
+								className="mb-2"
+								label="Phone Number"
+								type="tel"
+								value={formValues.patientPhoneNumber}
+								onChange={({ currentTarget }) => {
+									setFormValues((previousValue) => ({
+										...previousValue,
+										patientPhoneNumber: currentTarget.value,
+									}));
+								}}
+								disabled={isSaving}
+								required
+							/>
+							<InputHelper
+								className="mb-6"
+								label="Email Address"
+								type="email"
+								value={formValues.patientEmailAddress}
+								onChange={({ currentTarget }) => {
+									setFormValues((previousValue) => ({
+										...previousValue,
+										patientEmailAddress: currentTarget.value,
+									}));
+								}}
+								disabled={isSaving}
+								required
+							/>
+							<hr />
+						</Col>
+					</Row>
+					<Row className="mb-6">
+						<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+							<h4 className="mb-6">Care Preference</h4>
+							<Form.Group className="mb-6">
+								<h5 className="mb-2">
+									How would you prefer to connect with a mental health care provider?
+								</h5>
+								{carePreferences.map((carePreference) => (
+									<Form.Check
+										key={carePreference.patientOrderCarePreferenceId}
+										type="radio"
+										name="care-preference"
+										id={`care-preference--${carePreference.patientOrderCarePreferenceId}`}
+										label={carePreference.title}
+										value={carePreference.patientOrderCarePreferenceId}
+										checked={
+											formValues.patientOrderCarePreferenceId ===
+											carePreference.patientOrderCarePreferenceId
+										}
+										onChange={({ currentTarget }) => {
+											setFormValues((previousValue) => ({
+												...previousValue,
+												patientOrderCarePreferenceId: currentTarget.value,
+											}));
+										}}
+										disabled={isSaving}
 									/>
-									<hr className="mb-6" />
-									<h5 className="mb-6">Address</h5>
-									<PatientAddressFormInputs
-										//@ts-expect-error
-										formikProps={formikProps}
-										referenceData={referenceDataResponse}
+								))}
+							</Form.Group>
+							<Form.Group className="mb-6">
+								<h5 className="mb-2">
+									How far would you be willing to travel from your location (
+									{patientOrder?.patientAddress?.postalCode}) to see an in-person provider?
+								</h5>
+								{careRadii.map((careRadius) => (
+									<Form.Check
+										key={careRadius.inPersonCareRadiusId}
+										type="radio"
+										name="care-radius"
+										id={`care-radius--${careRadius.inPersonCareRadiusId}`}
+										label={careRadius.title}
+										value={careRadius.inPersonCareRadius}
+										checked={formValues.inPersonCareRadius === careRadius.inPersonCareRadius}
+										onChange={({ currentTarget }) => {
+											setFormValues((previousValue) => ({
+												...previousValue,
+												inPersonCareRadius: parseInt(currentTarget.value, 10),
+											}));
+										}}
+										disabled={isSaving}
 									/>
-									<hr className="mb-6" />
-									<h5 className="mb-2">About</h5>
-									<p className="mb-6">
-										This information is not required but can be helpful for your care team to know.{' '}
-									</p>
-									<PatientDemographicsFormInputs
-										//@ts-expect-error
-										formikProps={formikProps}
-										referenceData={referenceDataResponse}
-									/>
-									<div className="d-flex align-items-center justify-content-end">
-										<Button variant="primary" type="submit" disabled={isSaving}>
-											Continue
-										</Button>
-									</div>
-								</Form>
-							)}
-						</Formik>
-					</Col>
-				</Row>
+								))}
+							</Form.Group>
+							<hr />
+						</Col>
+					</Row>
+					<Row className="mb-6">
+						<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+							<h4 className="mb-2">About You</h4>
+							<p className="mb-6">
+								This information is not required but can be helpful for your care team to know.{' '}
+							</p>
+							<InputHelper
+								className="mb-2"
+								label="Preferred Language"
+								as="select"
+								value={formValues.patientLanguageCode}
+								onChange={({ currentTarget }) => {
+									setFormValues((previousValue) => ({
+										...previousValue,
+										patientLanguageCode: currentTarget.value,
+									}));
+								}}
+								disabled={isSaving}
+							>
+								<option value="">Select...</option>
+								{referenceDataResponse.languages.map((language) => {
+									return (
+										<option key={language.languageCode} value={language.languageCode}>
+											{language.description}
+										</option>
+									);
+								})}
+							</InputHelper>
+							<InputHelper
+								className="mb-2"
+								label="Race"
+								as="select"
+								value={formValues.patientRaceId}
+								onChange={({ currentTarget }) => {
+									setFormValues((previousValue) => ({
+										...previousValue,
+										patientRaceId: currentTarget.value,
+									}));
+								}}
+								disabled={isSaving}
+							>
+								<option value="">Select...</option>
+								{referenceDataResponse.races.map((race) => {
+									return (
+										<option key={race.raceId} value={race.raceId}>
+											{race.description}
+										</option>
+									);
+								})}
+							</InputHelper>
+							<InputHelper
+								className="mb-2"
+								label="Ethnicity"
+								as="select"
+								value={formValues.patientEthnicityId}
+								onChange={({ currentTarget }) => {
+									setFormValues((previousValue) => ({
+										...previousValue,
+										patientEthnicityId: currentTarget.value,
+									}));
+								}}
+								disabled={isSaving}
+							>
+								<option value="">Select...</option>
+								{referenceDataResponse.ethnicities.map((ethnicity) => {
+									return (
+										<option key={ethnicity.ethnicityId} value={ethnicity.ethnicityId}>
+											{ethnicity.description}
+										</option>
+									);
+								})}
+							</InputHelper>
+							<InputHelper
+								className="mb-2"
+								label="Birth Sex"
+								as="select"
+								value={formValues.patientBirthSexId}
+								onChange={({ currentTarget }) => {
+									setFormValues((previousValue) => ({
+										...previousValue,
+										patientBirthSexId: currentTarget.value,
+									}));
+								}}
+								disabled={isSaving}
+							>
+								<option value="">Select...</option>
+								{referenceDataResponse.birthSexes.map((birthSex) => {
+									return (
+										<option key={birthSex.birthSexId} value={birthSex.birthSexId}>
+											{birthSex.description}
+										</option>
+									);
+								})}
+							</InputHelper>
+							<InputHelper
+								className="mb-6"
+								label="Gender Identity"
+								as="select"
+								value={formValues.patientGenderIdentityId}
+								onChange={({ currentTarget }) => {
+									setFormValues((previousValue) => ({
+										...previousValue,
+										patientGenderIdentityId: currentTarget.value,
+									}));
+								}}
+								disabled={isSaving}
+							>
+								<option value="">Select...</option>
+								{referenceDataResponse.genderIdentities.map((genderIdentity) => {
+									return (
+										<option
+											key={genderIdentity.genderIdentityId}
+											value={genderIdentity.genderIdentityId}
+										>
+											{genderIdentity.description}
+										</option>
+									);
+								})}
+							</InputHelper>
+							<hr />
+						</Col>
+					</Row>
+					<Row className="mb-6">
+						<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+							<Form.Check
+								type="checkbox"
+								name="verify"
+								id="verify--information-correct"
+								label="I verify that all information entered is correct and complete to the best of my knowledge."
+								value="VERIFIED"
+								checked={formValues.verified}
+								onChange={({ currentTarget }) => {
+									setFormValues((previousValue) => ({
+										...previousValue,
+										verified: currentTarget.checked,
+									}));
+								}}
+								disabled={isSaving}
+							/>
+						</Col>
+					</Row>
+					<Row>
+						<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+							<div className="d-flex align-items-center justify-content-end">
+								<Button variant="primary" type="submit" disabled={isSaving}>
+									Continue
+								</Button>
+							</div>
+						</Col>
+					</Row>
+				</Form>
 			</Container>
 		</AsyncWrapper>
 	);
