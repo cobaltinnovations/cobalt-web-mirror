96948ddcfe37e509ef9c80ea85046cc383f73f75
Adam Grayson <asgrayson90@gmail.com>
2023-06-08 10:38:45 -0400

    logic to combine lists when required

--- src/lib/models/integrated-care-models.ts
+++ src/lib/models/integrated-care-models.ts
@@ -442,6 +442,7 @@ export interface PatientOrderScheduledMessageGroup {
 		messageTypeId: string;
 		messageTypeDescription: string;
 	}[];
+	scheduledAtDateTimeHasPassed: boolean;
 }
 
 export type PatientOrderCountsByPatientOrderTriageStatusId = Record<
