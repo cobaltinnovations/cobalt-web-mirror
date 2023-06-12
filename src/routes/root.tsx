ca82548647a4f4d295e37daa8afada5459a6d277
Khaled Shaaban <khaled.shaaban@xmog.com>
2023-06-12 12:18:08 -0400

    fix regression from data-router upgrade

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -108,13 +108,13 @@ export const Component = () => {
 	useUrlViewTracking();
 
 	return (
-		<AnalyticsProvider>
-			<AccountProvider>
+		<AccountProvider>
+			<AnalyticsProvider>
 				<BookingProvider>
 					<Layout />
 				</BookingProvider>
-			</AccountProvider>
-		</AnalyticsProvider>
+			</AnalyticsProvider>
+		</AccountProvider>
 	);
 };
 
