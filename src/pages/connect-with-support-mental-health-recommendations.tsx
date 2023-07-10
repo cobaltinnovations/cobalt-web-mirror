9466ac10d185a2ee91077f067da25054edb72948
Adam Grayson <asgrayson90@gmail.com>
2023-07-10 10:38:24 -0400

    recommendations page refactor

--- src/pages/connect-with-support-mental-health-recommendations.tsx
+++ src/pages/connect-with-support-mental-health-recommendations.tsx
@@ -3,27 +3,42 @@ import { useNavigate } from 'react-router-dom';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 
-import { AccountFeature } from '@/lib/models';
-import { accountService } from '@/lib/services';
+import { InstitutionFeature } from '@/lib/models';
+import { accountService, institutionService } from '@/lib/services';
 import useAccount from '@/hooks/use-account';
 import AsyncWrapper from '@/components/async-page';
 import NoData from '@/components/no-data';
-import { useBookingRequirementsNavigation } from '@/hooks/use-booking-requirements-navigation';
 
 const ConnectWithSupportMentalHealthRecommendations = () => {
 	const navigate = useNavigate();
-	const checkBookingRequirementsAndRedirect = useBookingRequirementsNavigation();
 	const { account, institution } = useAccount();
-	const [recommendedFeature, setRecommendedFeatures] = useState<AccountFeature[]>([]);
+	const [recommendedFeature, setRecommendedFeature] = useState<InstitutionFeature>();
+	const [myChartAuthUrl, setMyChartAuthUrl] = useState('');
 
 	const fetchData = useCallback(async () => {
 		if (!account?.accountId) {
 			throw new Error('accountId is undefined.');
 		}
 
-		const response = await accountService.getRecommendedFeatures(account.accountId).fetch();
-		setRecommendedFeatures(response.features);
-	}, [account?.accountId]);
+		const { features } = await accountService.getRecommendedFeatures(account.accountId).fetch();
+		const matchingInstitutionFeature = institution.features.find((f) => f.featureId === features[0]?.featureId);
+		setRecommendedFeature(matchingInstitutionFeature);
+
+		const { myChartConnectionRequired } = await accountService.getBookingRequirements(account.accountId).fetch();
+		if (!myChartConnectionRequired) {
+			return;
+		}
+
+		if (!institution.institutionId) {
+			throw new Error('institutionId is undefined.');
+		}
+
+		const { authenticationUrl } = await institutionService
+			.getMyChartAuthenticationUrl(institution.institutionId)
+			.fetch();
+
+		setMyChartAuthUrl(authenticationUrl);
+	}, [account?.accountId, institution.features, institution.institutionId]);
 
 	return (
 		<>
@@ -35,14 +50,14 @@ const ConnectWithSupportMentalHealthRecommendations = () => {
 				<Container className="py-20">
 					<Row>
 						<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
-							{recommendedFeature.length > 0 ? (
+							{recommendedFeature ? (
 								<>
 									<h1 className="mb-1">Assessment Results</h1>
 									<p className="mb-6 fs-large text-gray">Completed XXX</p>
 									<hr className="mb-8" />
 									<p className="mb-6 fs-large">
 										Based on the symptoms reported we recommend that you meet with a{' '}
-										<strong>{recommendedFeature[0]?.name}</strong>
+										<strong>{recommendedFeature.name}</strong>
 									</p>
 									<p className="mb-6 fs-large">
 										You can <strong>schedule a telehealth appointment</strong> with a Mental health
@@ -56,9 +71,18 @@ const ConnectWithSupportMentalHealthRecommendations = () => {
 										<Button
 											variant="primary"
 											size="lg"
-											onClick={checkBookingRequirementsAndRedirect}
+											onClick={() => {
+												if (myChartAuthUrl) {
+													window.open(myChartAuthUrl, '_blank', 'noopener, noreferrer');
+													return;
+												}
+
+												navigate(recommendedFeature.urlName);
+											}}
 										>
-											Connect to {institution.myChartName}
+											{myChartAuthUrl
+												? `Connect to ${institution.myChartName}`
+												: 'View Providers'}
 										</Button>
 									</div>
 								</>
