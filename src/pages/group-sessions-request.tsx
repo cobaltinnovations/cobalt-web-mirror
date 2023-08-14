ce63d6c6ac15c028e2b1d708d4f1dfee5e0288ae
Adam Grayson <asgrayson90@gmail.com>
2023-08-14 16:38:40 -0400

    update request form introduction

--- src/pages/group-sessions-request.tsx
+++ src/pages/group-sessions-request.tsx
@@ -160,11 +160,16 @@ const GroupSessionsRequest = () => {
 						<Col lg={{ span: 8, offset: 2 }}>
 							<h1 className="mb-6">Request a Group Session</h1>
 							<p className="mb-4">
-								Use this form to request a group session for your team (minimum of 5 people). Topics can
-								range from mindfulness to resilience to how to increase daily activity. Please request
-								your session at least two weeks in advance of the desired date. Upon submission of this
-								form, please allow 1-2 business days to hear back from the session coordinator. Sessions
-								are held virtually.
+								Please use this form to request a wellness-focused group session for your team (minimum
+								of 5 people). Also please request your session at least two weeks in advance of the
+								desired date. Upon submission, please allow 1-2 business days to hear back from the
+								session coordinator. Sessions are held virtually.
+							</p>
+							<p className="mb-4">
+								Selecting multiple topics of interest will increase the likelihood of booking a session
+								on the desired date, however this form is intended to schedule a single session for your
+								team. Please submit a separate form for an additional session, or add a note in the
+								comments section.
 							</p>
 							<p className="mb-0 text-danger">Required *</p>
 						</Col>
