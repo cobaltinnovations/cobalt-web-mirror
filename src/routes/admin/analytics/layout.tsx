cfa82ab918644523a7face1ea400c0e9d88aca1e
Khaled Shaaban <khaled.shaaban@xmog.com>
2023-08-22 16:37:55 -0400

    add fake/dummy data for analytic dashboards

--- src/routes/admin/analytics/layout.tsx
+++ src/routes/admin/analytics/layout.tsx
@@ -1,13 +1,13 @@
 import InputHelper from '@/components/input-helper';
 import Loader from '@/components/loader';
 import TabBar from '@/components/tab-bar';
-import moment from 'moment';
+import { adminAnalyticsService } from '@/lib/services/admin-analytics-service';
+import { ArcElement, BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Tooltip } from 'chart.js';
 import React, { Suspense, useMemo } from 'react';
 import { Col, Container, Row, Tab } from 'react-bootstrap';
-import { LoaderFunctionArgs, Outlet, useMatch, useRouteLoaderData, useSearchParams } from 'react-router-dom';
-import { ArcElement, Tooltip, Legend, Chart as ChartJS } from 'chart.js';
+import { Outlet, useMatch, useRouteLoaderData, useSearchParams } from 'react-router-dom';
 
-ChartJS.register(ArcElement, Tooltip, Legend);
+ChartJS.register(ArcElement, Tooltip, Legend, CategoryScale, LinearScale, BarElement);
 
 type AdminAnalyticsLayoutLoaderData = Awaited<ReturnType<typeof loader>>;
 
@@ -15,71 +15,15 @@ export function useAdminAnalyticsLayoutLoaderData() {
 	return useRouteLoaderData('admin-analytics-layout') as AdminAnalyticsLayoutLoaderData;
 }
 
-function getDateOptions() {
-	const today = moment();
-	const todayFormatted = today.format('MMM DD, YYYY');
-
-	const startOf7DaysAgo = moment().subtract(7, 'days').startOf('day');
-	const startOf7DaysAgoFormatted = startOf7DaysAgo.format('MMM DD, YYYY');
-	const startOf30DaysAgo = moment().subtract(30, 'days').startOf('day');
-	const startOf30DaysAgoFormatted = startOf30DaysAgo.format('MMM DD, YYYY');
-	const startOf90DaysAgo = moment().subtract(90, 'days').startOf('day');
-	const startOf90DaysAgoFormatted = startOf90DaysAgo.format('MMM DD, YYYY');
-	const startOf12MonthsAgo = moment().subtract(12, 'months').startOf('day');
-	const startOf12MonthsAgoFormatted = startOf12MonthsAgo.format('MMM DD, YYYY');
-	const startOfThisYear = moment().startOf('year').startOf('day');
-	const startOfThisYearFormatted = startOfThisYear.format('MMM DD, YYYY');
-
-	return [
-		{
-			label: 'Last 7 days',
-			startDate: startOf7DaysAgo.format('YYYY-MM-DD'),
-			startDateDescription: startOf7DaysAgoFormatted,
-			endDate: today.format('YYYY-MM-DD'),
-			endDateDescription: todayFormatted,
-		},
-		{
-			label: 'Last 30 days',
-			startDate: startOf30DaysAgo.format('YYYY-MM-DD'),
-			startDateDescription: startOf30DaysAgoFormatted,
-			endDate: today.format('YYYY-MM-DD'),
-			endDateDescription: todayFormatted,
-		},
-		{
-			label: 'Last 90 days',
-			startDate: startOf90DaysAgo.format('YYYY-MM-DD'),
-			startDateDescription: startOf90DaysAgoFormatted,
-			endDate: today.format('YYYY-MM-DD'),
-			endDateDescription: todayFormatted,
-		},
-		{
-			label: 'Last 12 months',
-			startDate: startOf12MonthsAgo.format('YYYY-MM-DD'),
-			startDateDescription: startOf12MonthsAgoFormatted,
-			endDate: today.format('YYYY-MM-DD'),
-			endDateDescription: todayFormatted,
-		},
-		{
-			label: 'This year',
-			startDate: startOfThisYear.format('YYYY-MM-DD'),
-			startDateDescription: startOfThisYearFormatted,
-			endDate: today.format('YYYY-MM-DD'),
-			endDateDescription: todayFormatted,
-		},
-	];
-}
-
-export async function loader({ request }: LoaderFunctionArgs) {
-	// const url = new URL(request.url);
-
+export async function loader() {
 	return {
-		dateOptions: getDateOptions(),
+		dateOptions: adminAnalyticsService.getDateOptions(),
 	};
 }
 
 export const Component = () => {
 	const { dateOptions } = useAdminAnalyticsLayoutLoaderData();
-	const match = useMatch('/admin/analytics/:tabId');
+	const match = useMatch('/admin/analytics/:dashboardTab');
 	const [searchParams, setSearchParams] = useSearchParams({
 		startDate: dateOptions[0].startDate,
 		endDate: dateOptions[0].endDate,
@@ -93,7 +37,7 @@ export const Component = () => {
 		});
 	}, [dateOptions, endDate, startDate]);
 
-	const paramTabKey = match?.params.tabId ?? 'overview';
+	const paramTabKey = match?.params.dashboardTab ?? 'overview';
 
 	return (
 		<Container fluid className="p-8">
