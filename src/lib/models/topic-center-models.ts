d31a564f0114640349145c0e35c10dbdbf072d18
Khaled Shaaban <khaled.shaaban@xmog.com>
2023-09-06 11:35:26 -0400

    home: featured topic center cta

--- src/lib/models/topic-center-models.ts
+++ src/lib/models/topic-center-models.ts
@@ -1,11 +1,21 @@
 import { GroupSessionModel, GroupSessionRequestModel, ResourceLibraryContentModel, TagModel } from '@/lib/models';
 
+export enum TopicCenterDisplayStyleId {
+	DEFAULT = 'DEFAULT',
+	FEATURED = 'FEATURED',
+}
+
 export interface TopicCenterModel {
 	name: string;
 	topicCenterId: string;
 	topicCenterRows: TopicCenterRowModel[];
 	urlName: string;
 	tagsByTagId: Record<string, TagModel>;
+	topicCenterDisplayStyleId: TopicCenterDisplayStyleId;
+	featuredTitle?: string;
+	featuredDescription?: string;
+	featuredCallToAction?: string;
+	imageUrl?: string;
 }
 
 export interface TopicCenterRowModel {
