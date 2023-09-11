478364401141bbb96eac485b68762e4af3446649
Khaled Shaaban <khaled.shaaban@xmog.com>
2023-09-11 16:01:31 -0400

    use `techSupportPhoneNumberDescription`

--- src/pages/connect-with-support-mental-health-providers.tsx
+++ src/pages/connect-with-support-mental-health-providers.tsx
@@ -11,7 +11,7 @@ const ConnectWithSupportMentalHealthProviders = () => {
 			renderFeatureDetail={(featureDetails) => {
 				return <p className="mb-0 text-center fs-large">{featureDetails.description}</p>;
 			}}
-			connectDescription={`In order to be connected with one of our providers, you will need to sign in to your ${institution.myChartName} account. If you do not have a ${institution.myChartName} account, click Learn More for instructions on how to sign up. If you have any difficulty getting an activation or access code, please contact ${institution.myChartName} technical support at ${institution.clinicalSupportPhoneNumberDescription}.`}
+			connectDescription={`In order to be connected with one of our providers, you will need to sign in to your ${institution.myChartName} account. If you do not have a ${institution.myChartName} account, click Learn More for instructions on how to sign up. If you have any difficulty getting an activation or access code, please contact ${institution.myChartName} technical support at ${institution.techSupportPhoneNumberDescription}.`}
 			connectEnabled
 		/>
 	);
