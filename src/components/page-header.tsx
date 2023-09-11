b6e9b2b84518914df1539c54812de472e97a7b93
Khaled Shaaban <khaled.shaaban@xmog.com>
2023-09-11 11:22:06 -0400

    page-header: wrap header container in container

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -13,24 +13,26 @@ interface PageHeaderProps {
 const PageHeader = ({ title, descriptionHtml, imageUrl, imageAlt, className }: PageHeaderProps) => {
 	return (
 		<Container fluid className={classNames('p-5 p-lg-16', className)}>
-			<Row>
-				<Col>
-					<h1 className="mb-6">{title}</h1>
-					{descriptionHtml && (
-						<div
-							dangerouslySetInnerHTML={{
-								__html: descriptionHtml,
-							}}
-						/>
-					)}
-				</Col>
-
-				{imageUrl && (
-					<Col xs={12} lg={{ offset: 1, span: 5 }} className="d-flex mt-12 mt-lg-0">
-						<img className="w-100 align-self-center" src={imageUrl} alt={imageAlt || title} />
+			<Container>
+				<Row>
+					<Col>
+						<h1 className="mb-6">{title}</h1>
+						{descriptionHtml && (
+							<div
+								dangerouslySetInnerHTML={{
+									__html: descriptionHtml,
+								}}
+							/>
+						)}
 					</Col>
-				)}
-			</Row>
+
+					{imageUrl && (
+						<Col xs={12} lg={{ offset: 1, span: 5 }} className="d-flex mt-12 mt-lg-0">
+							<img className="w-100 align-self-center" src={imageUrl} alt={imageAlt || title} />
+						</Col>
+					)}
+				</Row>
+			</Container>
 		</Container>
 	);
 };
