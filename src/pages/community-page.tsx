7590bd790e7773aabbbe0a4aab3507bb25b94f75
Khaled Shaaban <khaled.shaaban@xmog.com>
2023-10-19 13:40:14 -0400

    topic-center/community page group-session updates

--- src/pages/community-page.tsx
+++ src/pages/community-page.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import { useLocation, useNavigate, useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import { Col, Container, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
 
 import AsyncPage from '@/components/async-page';
-import HeroContainer from '@/components/hero-container';
-import { SkeletonTopicCenterGroupSession, TopicCenterGroupSession } from '@/components/topic-center-group-session';
+import { SkeletonTopicCenterGroupSession } from '@/components/topic-center-group-session';
 import { Masonry } from '@/components/masonry';
 import { TopicCenterPinboardItem } from '@/components/topic-center-pinboard-item';
-import classNames from 'classnames';
 import useAnalytics from '@/hooks/use-analytics';
 import { TopicCenterAnalyticsEvent } from '@/contexts/analytics-context';
 import ResourceLibraryCard, { SkeletonResourceLibraryCard } from '@/components/resource-library-card';
-import { SkeletonText } from '@/components/skeleton-loaders';
+import { SkeletonImage, SkeletonText } from '@/components/skeleton-loaders';
 import { GroupSessionDetailNavigationSource } from '@/routes/group-session-detail';
 import IneligibleBookingModal from '@/components/ineligible-booking-modal';
 import { useTopicCenterState } from '@/hooks/use-topic-center-state';
+import PageHeader from '@/components/page-header';
+import ResourceLibrarySubtopicCard from '@/components/resource-library-subtopic-card';
+import Carousel from '@/components/carousel';
+import { resourceLibraryCarouselConfig } from './resource-library';
 
 const CommunityPage = () => {
 	const { mixpanel, trackEvent } = useAnalytics();
-	const navigate = useNavigate();
 	const location = useLocation();
 	const { topicCenterId } = useParams<{ topicCenterId: string }>();
 	const { fetchData, topicCenter, trackContentEvent } = useTopicCenterState(topicCenterId);
@@ -36,9 +37,19 @@ const CommunityPage = () => {
 				fetchData={fetchData}
 				loadingComponent={
 					<>
-						<HeroContainer className="bg-p700">
-							<SkeletonText type="h1" numberOfLines={2} className="mb-0 text-center" />
-						</HeroContainer>
+						<Container fluid className="bg-p700 p-16">
+							<Row>
+								<Col xs={12} md={8}>
+									<SkeletonText type="h1" numberOfLines={1} />
+									<SkeletonText type="p" numberOfLines={3} />
+								</Col>
+
+								<Col xs={12} md={4}>
+									<SkeletonImage height={200} />
+								</Col>
+							</Row>
+						</Container>
+
 						<Container fluid className="bg-n50">
 							<Container className="pt-10 pb-12 pt-lg-14 pb-lg-22">
 								<Row>
@@ -114,18 +125,31 @@ const CommunityPage = () => {
 					</>
 				}
 			>
-				<HeroContainer className="bg-p700">
-					<h1 className="mb-0 text-white text-center">{topicCenter?.name}</h1>
-				</HeroContainer>
+				<PageHeader
+					className="bg-p700 text-white"
+					title={
+						<>
+							<p className="fs-large">Community</p>
+							<h1>{topicCenter?.name}</h1>
+						</>
+					}
+					descriptionHtml={topicCenter?.description}
+					imageUrl={topicCenter?.imageUrl}
+					imageAlt={topicCenter?.name}
+				/>
 
 				{topicCenter?.topicCenterRows.map((topicCenterRow, topicCenterRowIndex) => {
-					const backgroundColorClass = topicCenterRowIndex % 2 === 0 ? 'bg-n50' : 'bg-n75';
 					const containerClassNames = 'pb-12 pt-lg-14 pb-lg-22 pt-10';
+
+					const showScheduledGroupSessionSection = topicCenterRow.groupSessions.length > 0;
+					const showByRequestGroupSessionSection = topicCenterRow.groupSessionRequests.length > 0;
+					const showGroupSessionSection =
+						showScheduledGroupSessionSection || showByRequestGroupSessionSection;
 
 					return (
 						<React.Fragment key={topicCenterRow.topicCenterRowId}>
-							{topicCenterRow.groupSessions.length > 0 && (
-								<Container fluid className={backgroundColorClass} key={topicCenterRow.topicCenterRowId}>
+							{showGroupSessionSection && (
+								<Container fluid className="bg-n50" key={topicCenterRow.topicCenterRowId}>
 									<Container className={containerClassNames}>
 										<Row>
 											<Col
@@ -140,146 +164,139 @@ const CommunityPage = () => {
 											</Col>
 										</Row>
 
-										<Row>
-											<Col
-												md={{ span: 10, offset: 1 }}
-												lg={{ span: 10, offset: 1 }}
-												xl={{ span: 8, offset: 2 }}
-											>
-												{topicCenterRow.groupSessions.map((groupSession, groupSessionIndex) => {
-													const isLast =
-														topicCenterRow.groupSessions.length - 1 === groupSessionIndex;
+										{showScheduledGroupSessionSection && (
+											<Row className="mb-11 mb-lg-18">
+												<Col lg={3} className="mb-10 mb-lg-0 pt-4 pb-2">
+													<ResourceLibrarySubtopicCard
+														className="h-100"
+														title="Scheduled"
+														description="TODO: Description specific to Scheduled Group Sessions related to current Topic"
+													/>
+												</Col>
 
-													return (
-														<TopicCenterGroupSession
-															key={groupSession.groupSessionId}
-															className={classNames({
-																'mb-8': !isLast,
-															})}
-															title={groupSession.title}
-															titleSecondary={groupSession.appointmentTimeDescription}
-															titleTertiary={`Hosted by: ${groupSession.facilitatorName}`}
-															description={groupSession.description}
-															badgeTitle={
-																groupSession.seatsAvailable &&
-																groupSession.seatsAvailable <= 20
-																	? groupSession.seatsAvailableDescription
-																	: ''
-															}
-															buttonTitle="Reserve a Place"
-															onClick={() => {
-																trackEvent(
-																	TopicCenterAnalyticsEvent.clickGroupSession(
-																		topicCenter.name,
-																		topicCenterRow.title
-																	)
+												<Col lg={9}>
+													<Carousel
+														responsive={resourceLibraryCarouselConfig}
+														trackStyles={{ paddingTop: 16, paddingBottom: 8 }}
+														floatingButtonGroup
+													>
+														{topicCenterRow.groupSessions.map((groupSession) => {
+															return (
+																<ResourceLibraryCard
+																	key={groupSession.groupSessionId}
+																	linkTo={`/group-sessions/${groupSession.urlName}`}
+																	linkToOptions={{
+																		state: {
+																			navigationSource:
+																				GroupSessionDetailNavigationSource.TOPIC_CENTER,
+																			topicCenterPath: location.pathname,
+																		},
+																	}}
+																	className="h-100"
+																	imageUrl={groupSession.imageUrl}
+																	title={groupSession.title}
+																	subtitle={groupSession.startDateTimeDescription}
+																	authorPrefix="Hosted by"
+																	author={groupSession.facilitatorName}
+																	tags={[]}
+																	trackEvent={() => {
+																		trackEvent(
+																			TopicCenterAnalyticsEvent.clickGroupSession(
+																				topicCenter.name,
+																				topicCenterRow.title
+																			)
+																		);
+
+																		mixpanel.track(
+																			'Topic Center Group Session Click',
+																			{
+																				'Topic Center ID':
+																					topicCenter.topicCenterId,
+																				'Topic Center Title': topicCenter.name,
+																				'Section Title': topicCenterRow.title,
+																				'Group Session ID':
+																					groupSession.groupSessionId,
+																				'Group Session Title':
+																					groupSession.title,
+																				'Group Session Start Time':
+																					groupSession.startDateTime,
+																			}
+																		);
+																	}}
+																/>
+															);
+														})}
+													</Carousel>
+												</Col>
+											</Row>
+										)}
+
+										{showByRequestGroupSessionSection && (
+											<Row className="mb-11 mb-lg-18">
+												<Col lg={3} className="mb-10 mb-lg-0 pt-4 pb-2">
+													<ResourceLibrarySubtopicCard
+														className="h-100"
+														title="By Request"
+														description="TODO: Description specific to By Request Group Sessions related to current Topic"
+													/>
+												</Col>
+
+												<Col lg={9}>
+													<Carousel
+														responsive={resourceLibraryCarouselConfig}
+														trackStyles={{ paddingTop: 16, paddingBottom: 8 }}
+														floatingButtonGroup
+													>
+														{topicCenterRow.groupSessionRequests.map(
+															(groupSessionRequest) => {
+																return (
+																	<ResourceLibraryCard
+																		key={groupSessionRequest.groupSessionRequestId}
+																		linkTo={`/in-the-studio/group-session-by-request/${groupSessionRequest.groupSessionRequestId}`}
+																		className="h-100"
+																		imageUrl={groupSessionRequest.imageUrl}
+																		title={groupSessionRequest.title}
+																		authorPrefix="BY REQUEST"
+																		author={''}
+																		tags={[]}
+																		trackEvent={() => {
+																			trackEvent(
+																				TopicCenterAnalyticsEvent.clickGroupSessionByRequest(
+																					topicCenter.name,
+																					topicCenterRow.title
+																				)
+																			);
+
+																			mixpanel.track(
+																				'Topic Center Group Session By Request Click',
+																				{
+																					'Topic Center ID':
+																						topicCenter.topicCenterId,
+																					'Topic Center Title':
+																						topicCenter.name,
+																					'Section Title':
+																						topicCenterRow.title,
+																					'Group Session By Request ID':
+																						groupSessionRequest.groupSessionRequestId,
+																					'Group Session By Request Title':
+																						groupSessionRequest.title,
+																				}
+																			);
+																		}}
+																	/>
 																);
-
-																mixpanel.track('Topic Center Group Session Click', {
-																	'Topic Center ID': topicCenter.topicCenterId,
-																	'Topic Center Title': topicCenter.name,
-																	'Section Title': topicCenterRow.title,
-																	'Group Session ID': groupSession.groupSessionId,
-																	'Group Session Title': groupSession.title,
-																	'Group Session Start Time':
-																		groupSession.startDateTime,
-																});
-
-																navigate(`/group-sessions/${groupSession.urlName}`, {
-																	state: {
-																		navigationSource:
-																			GroupSessionDetailNavigationSource.TOPIC_CENTER,
-																		topicCenterPath: location.pathname,
-																	},
-																});
-															}}
-															imageUrl={groupSession.imageUrl}
-														/>
-													);
-												})}
-											</Col>
-										</Row>
-									</Container>
-								</Container>
-							)}
-
-							{topicCenterRow.groupSessionRequests.length > 0 && (
-								<Container fluid className={backgroundColorClass} key={topicCenterRow.topicCenterRowId}>
-									<Container className={containerClassNames}>
-										<Row>
-											<Col
-												md={{ span: 10, offset: 1 }}
-												lg={{ span: 8, offset: 2 }}
-												xl={{ span: 6, offset: 3 }}
-											>
-												<h2 className="mb-2 mb-lg-4 text-center">{topicCenterRow.title}</h2>
-												<p className="mb-6 mb-lg-12 fs-large text-center">
-													{topicCenterRow.description}
-												</p>
-											</Col>
-										</Row>
-
-										<Row>
-											<Col
-												md={{ span: 10, offset: 1 }}
-												lg={{ span: 10, offset: 1 }}
-												xl={{ span: 8, offset: 2 }}
-											>
-												{topicCenterRow.groupSessionRequests.map(
-													(groupSessionRequest, groupSessionRequestIndex) => {
-														const isLast =
-															topicCenterRow.groupSessionRequests.length - 1 ===
-															groupSessionRequestIndex;
-
-														return (
-															<TopicCenterGroupSession
-																key={groupSessionRequest.groupSessionRequestId}
-																className={classNames({
-																	'mb-8': !isLast,
-																})}
-																title={groupSessionRequest.title}
-																titleSecondary="By Request"
-																description={groupSessionRequest.description}
-																buttonTitle="Submit a Request"
-																onClick={() => {
-																	trackEvent(
-																		TopicCenterAnalyticsEvent.clickGroupSessionByRequest(
-																			topicCenter.name,
-																			topicCenterRow.title
-																		)
-																	);
-
-																	mixpanel.track(
-																		'Topic Center Group Session By Request Click',
-																		{
-																			'Topic Center ID':
-																				topicCenter.topicCenterId,
-																			'Topic Center Title': topicCenter.name,
-																			'Section Title': topicCenterRow.title,
-																			'Group Session By Request ID':
-																				groupSessionRequest.groupSessionRequestId,
-																			'Group Session By Request Title':
-																				groupSessionRequest.title,
-																		}
-																	);
-
-																	navigate(
-																		`/in-the-studio/group-session-by-request/${groupSessionRequest.groupSessionRequestId}`
-																	);
-																}}
-																imageUrl={groupSessionRequest.imageUrl}
-															/>
-														);
-													}
-												)}
-											</Col>
-										</Row>
+															}
+														)}
+													</Carousel>
+												</Col>
+											</Row>
+										)}
 									</Container>
 								</Container>
 							)}
 
 							{topicCenterRow.pinboardNotes.length > 0 && (
-								<Container fluid className={backgroundColorClass} key={topicCenterRow.topicCenterRowId}>
+								<Container fluid className="bg-n50" key={topicCenterRow.topicCenterRowId}>
 									<Container fluid="lg" className={containerClassNames}>
 										<Row>
 											<Col
@@ -316,7 +333,7 @@ const CommunityPage = () => {
 							)}
 
 							{topicCenterRow.contents.length > 0 && (
-								<Container fluid className={backgroundColorClass} key={topicCenterRow.topicCenterRowId}>
+								<Container fluid className="bg-n50" key={topicCenterRow.topicCenterRowId}>
 									<Container className={containerClassNames}>
 										<Row>
 											<Col
@@ -344,7 +361,7 @@ const CommunityPage = () => {
 													>
 														<ResourceLibraryCard
 															key={content.contentId}
-															contentId={content.contentId}
+															linkTo={`/resource-library/${content.contentId}`}
 															className="h-100"
 															imageUrl={content.imageUrl}
 															badgeTitle={content.newFlag ? 'New' : ''}
