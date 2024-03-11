8cef3f4a38312029775a3757ce4196ebea5429d3
Adam Grayson <asgrayson90@gmail.com>
2024-03-11 09:41:54 -0400

    IC specific confirm page

--- src/pages/confirm-appointment.tsx
+++ src/pages/confirm-appointment.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { Navigate, useMatch, useNavigate, useSearchParams } from 'react-router-dom';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
@@ -12,6 +12,8 @@ import InputHelper from '@/components/input-helper';
 import AppointmentUnavailableModal from '@/components/appointment-unavailable-modal';
 import { CobaltError } from '@/lib/http-client';
 import Cookies from 'js-cookie';
+import moment from 'moment';
+import { DateFormats } from '@/lib/utils';
 
 const ConfirmAppointment = () => {
 	const { institution } = useAccount();
@@ -25,6 +27,8 @@ const ConfirmAppointment = () => {
 	const intakeAssessmentId = searchParams.get('intakeAssessmentId') ?? undefined;
 	const patientOrderId = searchParams.get('patientOrderId') ?? undefined;
 	const epicAppointmentFhirId = searchParams.get('epicAppointmentFhirId') ?? undefined;
+	const formattedTime = useMemo(() => moment(time, 'HH:mm').format(DateFormats.UI.TimeSlotInput), [time]);
+	const formattedDate = useMemo(() => moment(date, 'YYYY-MM-DD').format(DateFormats.UI.Date), [date]);
 
 	const { addFlag } = useFlags();
 	const { account, isIntegratedCarePatient } = useAccount();
@@ -247,98 +251,120 @@ const ConfirmAppointment = () => {
 				}}
 			/>
 
-			<AsyncPage fetchData={fetchData}>
+			{institution.integratedCareEnabled ? (
 				<Container className="py-6">
 					<Row>
 						<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
-							{!confirmationCodeRequested && (
-								<>
-									<h2 className="mb-4">
-										{promptForPhoneNumber
-											? 'Provide an email address and phone number to finish booking your appointment'
-											: 'Provide an email address to finish booking your appointment'}
-									</h2>
-
-									<p className="mb-6">
-										We need your contact information to send appointment details and will also share
-										your name and email address with your appointment provider. Please make sure
-										that your email address is entered correctly.
-									</p>
-
-									<Form onSubmit={handleContactInformationFormSubmit}>
-										<InputHelper
-											required
-											className={promptForPhoneNumber ? 'mb-2' : 'mb-6'}
-											type="email"
-											value={emailInputValue}
-											label="Email Address"
-											autoFocus
-											onChange={({ currentTarget }) => {
-												setEmailInputValue(currentTarget.value);
-											}}
-											disabled={submitting}
-										/>
-										{promptForPhoneNumber && (
-											<InputHelper
-												required
-												className="mb-6"
-												type="tel"
-												value={phoneNumberInputValue}
-												label="Your Phone Number"
-												onChange={({ currentTarget }) => {
-													setPhoneNumberInputValue(currentTarget.value);
-												}}
-												disabled={submitting}
-											/>
-										)}
-										<div className="text-right">
-											<Button type="submit" size="lg" disabled={submitting}>
-												Continue
-											</Button>
-										</div>
-									</Form>
-								</>
-							)}
-							{confirmationCodeRequested && (
-								<>
-									<h2 className="mb-4">We need to verify your email address.</h2>
-									<p className="mb-6">
-										Enter the confirmation code that was sent to {emailInputValue} to finish booking
-										your appointment.
-									</p>
-									<Form onSubmit={handleConfirmationCodeFormSubmit}>
-										<InputHelper
-											required
-											className="mb-6"
-											type="text"
-											value={confirmationCodeInputValue}
-											label="Confirmation Code"
-											onChange={({ currentTarget }) => {
-												setConfirmationCodeInputValue(currentTarget.value);
-											}}
-											disabled={submitting}
-										/>
-										<div className="text-right">
-											<Button
-												className="me-4"
-												variant="light"
-												size="lg"
-												onClick={handleResendCodeButtonClick}
-												disabled={submitting}
-											>
-												Resend Code
-											</Button>
-											<Button type="submit" size="lg" disabled={submitting}>
-												Continue
-											</Button>
-										</div>
-									</Form>
-								</>
-							)}
+							<h2 className="mb-4">Finish booking your appointment</h2>
+							<p className="mb-6">
+								Your appointment will be scheduled for {formattedTime} on {formattedDate}. If this is
+								OK, press the Book Appointment button below. If you have any trouble booking your
+								appointment, please call the {institution.integratedCareProgramName} Resource Center at{' '}
+								{institution.integratedCarePhoneNumberDescription}{' '}
+								{institution.integratedCareAvailabilityDescription}.
+							</p>
+							<div className="text-right">
+								<Button size="lg" onClick={createAppointmentAndNavigate} disabled={submitting}>
+									Book Appointment
+								</Button>
+							</div>
 						</Col>
 					</Row>
 				</Container>
-			</AsyncPage>
+			) : (
+				<AsyncPage fetchData={fetchData}>
+					<Container className="py-6">
+						<Row>
+							<Col md={{ span: 10, offset: 1 }} lg={{ span: 8, offset: 2 }} xl={{ span: 6, offset: 3 }}>
+								{!confirmationCodeRequested && (
+									<>
+										<h2 className="mb-4">
+											{promptForPhoneNumber
+												? 'Provide an email address and phone number to finish booking your appointment'
+												: 'Provide an email address to finish booking your appointment'}
+										</h2>
+
+										<p className="mb-6">
+											We need your contact information to send appointment details and will also
+											share your name and email address with your appointment provider. Please
+											make sure that your email address is entered correctly.
+										</p>
+
+										<Form onSubmit={handleContactInformationFormSubmit}>
+											<InputHelper
+												required
+												className={promptForPhoneNumber ? 'mb-2' : 'mb-6'}
+												type="email"
+												value={emailInputValue}
+												label="Email Address"
+												autoFocus
+												onChange={({ currentTarget }) => {
+													setEmailInputValue(currentTarget.value);
+												}}
+												disabled={submitting}
+											/>
+											{promptForPhoneNumber && (
+												<InputHelper
+													required
+													className="mb-6"
+													type="tel"
+													value={phoneNumberInputValue}
+													label="Your Phone Number"
+													onChange={({ currentTarget }) => {
+														setPhoneNumberInputValue(currentTarget.value);
+													}}
+													disabled={submitting}
+												/>
+											)}
+											<div className="text-right">
+												<Button type="submit" size="lg" disabled={submitting}>
+													Continue
+												</Button>
+											</div>
+										</Form>
+									</>
+								)}
+								{confirmationCodeRequested && (
+									<>
+										<h2 className="mb-4">We need to verify your email address.</h2>
+										<p className="mb-6">
+											Enter the confirmation code that was sent to {emailInputValue} to finish
+											booking your appointment.
+										</p>
+										<Form onSubmit={handleConfirmationCodeFormSubmit}>
+											<InputHelper
+												required
+												className="mb-6"
+												type="text"
+												value={confirmationCodeInputValue}
+												label="Confirmation Code"
+												onChange={({ currentTarget }) => {
+													setConfirmationCodeInputValue(currentTarget.value);
+												}}
+												disabled={submitting}
+											/>
+											<div className="text-right">
+												<Button
+													className="me-4"
+													variant="light"
+													size="lg"
+													onClick={handleResendCodeButtonClick}
+													disabled={submitting}
+												>
+													Resend Code
+												</Button>
+												<Button type="submit" size="lg" disabled={submitting}>
+													Continue
+												</Button>
+											</div>
+										</Form>
+									</>
+								)}
+							</Col>
+						</Row>
+					</Container>
+				</AsyncPage>
+			)}
 		</>
 	);
 };
