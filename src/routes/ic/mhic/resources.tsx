dc6d2757de188bf34f583d31f83ae324d4481e68
Adam Grayson <asgrayson90@gmail.com>
2024-10-15 09:42:21 -0400

    refactor table data call to internal

--- src/routes/ic/mhic/resources.tsx
+++ src/routes/ic/mhic/resources.tsx
@@ -1,26 +1,17 @@
-import React, { Suspense, useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Helmet } from 'react-helmet';
-import {
-	Await,
-	defer,
-	Link,
-	LoaderFunctionArgs,
-	Outlet,
-	useLocation,
-	useMatches,
-	useNavigate,
-	useRouteLoaderData,
-	useSearchParams,
-} from 'react-router-dom';
+import { Link, Outlet, useLocation, useMatches, useNavigate, useSearchParams } from 'react-router-dom';
 import { Badge, Button, Col, Container, Dropdown, Offcanvas, Row } from 'react-bootstrap';
+import { CareResourceModel } from '@/lib/models';
+import { careResourceService } from '@/lib/services';
+import useHandleError from '@/hooks/use-handle-error';
 import { MhicPageHeader } from '@/components/integrated-care/mhic';
 import { Table, TableBody, TableCell, TableHead, TablePagination, TableRow } from '@/components/table';
 import { DropdownMenu, DropdownToggle } from '@/components/dropdown';
+import { createUseThemedStyles } from '@/jss/theme';
 import { ReactComponent as PlusIcon } from '@/assets/icons/icon-plus.svg';
 import { ReactComponent as CopyIcon } from '@/assets/icons/icon-content-copy.svg';
 import { ReactComponent as MoreIcon } from '@/assets/icons/more-horiz.svg';
-import { createUseThemedStyles } from '@/jss/theme';
-import { careResourceService, GetCareResourcesResponseModel } from '@/lib/services';
 
 const useStyles = createUseThemedStyles((theme) => ({
 	shelf: {
@@ -33,32 +24,51 @@ const useStyles = createUseThemedStyles((theme) => ({
 	},
 }));
 
-export function useCareResourcesLoader() {
-	return useRouteLoaderData('care-resources') as { getCareResourcesPromise: Promise<GetCareResourcesResponseModel> };
-}
-
-export const loader = async ({ request }: LoaderFunctionArgs) => {
-	const url = new URL(request.url);
-	const pageNumber = url.searchParams.get('pageNumber');
-	const pageSize = url.searchParams.get('pageSize');
-
-	return defer({
-		getCareResourcesPromise: careResourceService
-			.getCareResources({
-				...(pageNumber && { pageNumber }),
-				...(pageSize && { pageSize }),
-			})
-			.fetch(),
-	});
+// TODO: Use this page blocking loader to fetch the filters
+export const loader = async () => {
+	return null;
 };
 
 export const Component = () => {
-	const { getCareResourcesPromise } = useCareResourcesLoader();
 	const [searchParams, setSearchParams] = useSearchParams();
+	const pageNumber = useMemo(() => searchParams.get('pageNumber') ?? '', [searchParams]);
+	const pageSize = useMemo(() => searchParams.get('pageSize') ?? '', [searchParams]);
+
 	const classes = useStyles();
 	const location = useLocation();
 	const navigate = useNavigate();
 	const matches = useMatches();
+
+	const handleError = useHandleError();
+	const [isLoading, setIsLoading] = useState(false);
+	const [careResources, setCareResources] = useState<CareResourceModel[]>([]);
+	const [careResourcesTotalCount, setCareResourcesTotalCount] = useState(0);
+	const [careResourcesTotalCountDescription, setCareResourcesTotalCountDescription] = useState('0');
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				setIsLoading(true);
+
+				const response = await careResourceService
+					.getCareResources({
+						...(pageNumber && { pageNumber }),
+						...(pageSize && { pageSize }),
+					})
+					.fetch();
+
+				setCareResources(response.careResources.careResources);
+				setCareResourcesTotalCount(response.careResources.totalCount);
+				setCareResourcesTotalCountDescription(response.careResources.totalCountDescription);
+			} catch (error) {
+				handleError(error);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+
+		fetchData();
+	}, [handleError, pageNumber, pageSize]);
 
 	return (
 		<>
@@ -105,115 +115,104 @@ export const Component = () => {
 						</div>
 					</Col>
 				</Row>
-				<Suspense>
-					<Await resolve={getCareResourcesPromise}>
-						{({ careResources }: GetCareResourcesResponseModel) => (
-							<>
-								<Row className="mb-8">
-									<Col>
-										<Table isLoading={false}>
-											<TableHead>
-												<TableRow>
-													<TableCell header>Name</TableCell>
-													<TableCell header>Therapy Type</TableCell>
-													<TableCell header>ZIP</TableCell>
-													<TableCell header>Availability</TableCell>
-													<TableCell></TableCell>
-												</TableRow>
-											</TableHead>
-											<TableBody>
-												{careResources.careResources.map((careResource) => (
-													<TableRow key={careResource.carResourceId}>
-														<TableCell>
-															<Link
-																to={`${location.pathname}/${careResource.carResourceId}`}
-															>
-																{careResource.name}
-															</Link>
-														</TableCell>
-														<TableCell>Psychiatry</TableCell>
-														<TableCell>19444, 19428</TableCell>
-														<TableCell className="flex-row align-items-center justify-content-start">
-															<Badge pill bg="outline-success">
-																Available
-															</Badge>
-														</TableCell>
-														<TableCell className="flex-row align-items-center justify-content-end">
-															<Button
-																variant="light"
-																className="p-2 me-2"
-																onClick={(event) => {
-																	event.stopPropagation();
-																}}
-															>
-																<CopyIcon className="d-flex" width={20} height={20} />
-															</Button>
-															<Dropdown>
-																<Dropdown.Toggle
-																	as={DropdownToggle}
-																	id={`mhic-resources__dropdown-menu--${'resource-0'}`}
-																	className="p-2"
-																>
-																	<MoreIcon className="d-flex" />
-																</Dropdown.Toggle>
-																<Dropdown.Menu
-																	as={DropdownMenu}
-																	align="end"
-																	popperConfig={{ strategy: 'fixed' }}
-																	renderOnMount
-																>
-																	<Dropdown.Item
-																		onClick={() => {
-																			return;
-																		}}
-																	>
-																		Action 1
-																	</Dropdown.Item>
-																	<Dropdown.Item
-																		onClick={() => {
-																			return;
-																		}}
-																	>
-																		Action 2
-																	</Dropdown.Item>
-																</Dropdown.Menu>
-															</Dropdown>
-														</TableCell>
-													</TableRow>
-												))}
-											</TableBody>
-										</Table>
-									</Col>
-								</Row>
-								<Row>
-									<Col xs={{ span: 4, offset: 4 }}>
-										<div className="d-flex justify-content-center align-items-center">
-											<TablePagination
-												total={careResources.totalCount ?? 0}
-												page={0}
-												size={10}
-												onClick={(pageIndex) => {
-													searchParams.set('pageNumber', String(pageIndex));
-													setSearchParams(searchParams);
+
+				<Row className="mb-8">
+					<Col>
+						<Table isLoading={isLoading}>
+							<TableHead>
+								<TableRow>
+									<TableCell header>Name</TableCell>
+									<TableCell header>Therapy Type</TableCell>
+									<TableCell header>ZIP</TableCell>
+									<TableCell header>Availability</TableCell>
+									<TableCell></TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{careResources.map((careResource) => (
+									<TableRow key={careResource.carResourceId}>
+										<TableCell>
+											<Link to={`${location.pathname}/${careResource.carResourceId}`}>
+												{careResource.name}
+											</Link>
+										</TableCell>
+										<TableCell>Psychiatry</TableCell>
+										<TableCell>19444, 19428</TableCell>
+										<TableCell className="flex-row align-items-center justify-content-start">
+											<Badge pill bg="outline-success">
+												Available
+											</Badge>
+										</TableCell>
+										<TableCell className="flex-row align-items-center justify-content-end">
+											<Button
+												variant="light"
+												className="p-2 me-2"
+												onClick={(event) => {
+													event.stopPropagation();
 												}}
-											/>
-										</div>
-									</Col>
-									<Col xs={4}>
-										<div className="d-flex justify-content-end align-items-center">
-											<p className="mb-0 fw-semibold text-gray">
-												<span className="text-dark">{careResources.careResources.length}</span>{' '}
-												of{' '}
-												<span className="text-dark">{careResources.totalCountDescription}</span>{' '}
-												Care Resources
-											</p>
-										</div>
-									</Col>
-								</Row>
-							</>
-						)}
-					</Await>
-				</Suspense>
+											>
+												<CopyIcon className="d-flex" width={20} height={20} />
+											</Button>
+											<Dropdown>
+												<Dropdown.Toggle
+													as={DropdownToggle}
+													id={`mhic-resources__dropdown-menu--${'resource-0'}`}
+													className="p-2"
+												>
+													<MoreIcon className="d-flex" />
+												</Dropdown.Toggle>
+												<Dropdown.Menu
+													as={DropdownMenu}
+													align="end"
+													popperConfig={{ strategy: 'fixed' }}
+													renderOnMount
+												>
+													<Dropdown.Item
+														onClick={() => {
+															return;
+														}}
+													>
+														Action 1
+													</Dropdown.Item>
+													<Dropdown.Item
+														onClick={() => {
+															return;
+														}}
+													>
+														Action 2
+													</Dropdown.Item>
+												</Dropdown.Menu>
+											</Dropdown>
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</Col>
+				</Row>
+				<Row>
+					<Col xs={{ span: 4, offset: 4 }}>
+						<div className="d-flex justify-content-center align-items-center">
+							<TablePagination
+								total={careResourcesTotalCount}
+								page={parseInt(pageNumber, 10)}
+								size={parseInt(pageSize, 10)}
+								onClick={(pageIndex) => {
+									searchParams.set('pageNumber', String(pageIndex));
+									setSearchParams(searchParams);
+								}}
+							/>
+						</div>
+					</Col>
+					<Col xs={4}>
+						<div className="d-flex justify-content-end align-items-center">
+							<p className="mb-0 fw-semibold text-gray">
+								<span className="text-dark">{careResources.length}</span> of{' '}
+								<span className="text-dark">{careResourcesTotalCountDescription}</span> Care Resources
+							</p>
+						</div>
+					</Col>
+				</Row>
 			</Container>
 
 			<Offcanvas
