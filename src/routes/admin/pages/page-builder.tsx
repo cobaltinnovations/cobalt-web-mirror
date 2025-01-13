e3b5c6913921457bd822de2e4c2dc935aa7341ab
Adam Grayson <asgrayson90@gmail.com>
2025-01-13 10:56:20 -0500

    panel layout

--- src/routes/admin/pages/page-builder.tsx
+++ src/routes/admin/pages/page-builder.tsx
@@ -1,38 +1,89 @@
-import { MhicFullscreenBar } from '@/components/integrated-care/mhic';
 import React from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import { createUseThemedStyles } from '@/jss/theme';
+import PageHeader from '@/components/page-header';
+
+const useStyles = createUseThemedStyles((theme) => ({
+	wrapper: {
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
+		zIndex: 0,
+		position: 'fixed',
+	},
+	header: {
+		top: 0,
+		left: 0,
+		right: 0,
+		height: 60,
+		zIndex: 1,
+		position: 'absolute',
+		backgroundColor: theme.colors.n0,
+		borderBottom: `1px solid ${theme.colors.n100}`,
+	},
+	aside: {
+		top: 60,
+		left: 0,
+		bottom: 0,
+		width: 376,
+		zIndex: 1,
+		position: 'absolute',
+		backgroundColor: theme.colors.n0,
+		borderRight: `1px solid ${theme.colors.n100}`,
+	},
+	previewPane: {
+		top: 60,
+		left: 376,
+		right: 0,
+		bottom: 0,
+		zIndex: 1,
+		padding: 24,
+		overflowY: 'auto',
+		position: 'absolute',
+		backgroundColor: theme.colors.n75,
+	},
+	previewPage: {
+		borderRadius: 8,
+		overflow: 'hidden',
+		backgroundColor: theme.colors.n50,
+		border: `1px solid ${theme.colors.n100}`,
+	},
+}));
 
 export async function loader() {
 	return null;
 }
 
 export const Component = () => {
-	const navigate = useNavigate();
+	const classes = useStyles();
 
 	return (
-		<>
+		<div className={classes.wrapper}>
 			{/* path matching logic in components/admin/admin-header.tsx hides the default header */}
-			<MhicFullscreenBar
-				showExitButton={false}
-				title="Page Name"
-				primaryAction={{
-					title: 'Publish',
-					onClick: () => navigate(-1),
-				}}
-				tertiaryAction={{
-					title: 'Finish Later',
-					onClick: () => navigate(-1),
-				}}
-			/>
-
-			<Container fluid className="px-8 py-8">
-				<Row className="mb-6">
-					<Col>
-						<h2 className="mb-0">PageBuilder</h2>
-					</Col>
-				</Row>
-			</Container>
-		</>
+			<div className={classes.header}></div>
+			<div className={classes.aside}></div>
+			<div className={classes.previewPane}>
+				<div className={classes.previewPage}>
+					<PageHeader
+						className="bg-p700 text-white"
+						title={
+							<>
+								<p className="fs-large">Community</p>
+								<h1>Name</h1>
+							</>
+						}
+						descriptionHtml="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed placerat
+											consectetur magna, a pretium purus mattis et. Proin sagittis ex a faucibus
+											pellentesque. Sed posuere neque vel elementum rutrum. Vivamus faucibus
+											blandit nibh ut sodales. Quisque at enim fringilla, fringilla massa sed,
+											porttitor lectus. Morbi ut aliquam purus, sit amet interdum massa. Etiam ac
+											maximus ante. In fermentum dolor in aliquam venenatis. Sed sit amet laoreet
+											ante."
+						//imageUrl={topicCenter?.imageUrl}
+						//imageAlt={topicCenter?.name}
+					/>
+				</div>
+			</div>
+		</div>
 	);
 };
