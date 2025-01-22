7c4c3f24bd6104da9d10634fd172e9da430eea9c
Adam Grayson <asgrayson90@gmail.com>
2025-01-22 12:15:10 -0500

    API call for resources modal

--- src/components/admin/pages/select-resources-modal.tsx
+++ src/components/admin/pages/select-resources-modal.tsx
@@ -1,8 +1,12 @@
-import React from 'react';
-import { Button, Modal, ModalProps } from 'react-bootstrap';
+import React, { useState } from 'react';
+import { Button, Form, Modal, ModalProps } from 'react-bootstrap';
 import { createUseThemedStyles } from '@/jss/theme';
 import InputHelperSearch from '@/components/input-helper-search';
-import MegaFilter, { Filter } from '@/components/mega-filter';
+import MegaFilter from '@/components/mega-filter';
+import { resourceLibraryService } from '@/lib/services';
+import useHandleError from '@/hooks/use-handle-error';
+import { Content } from '@/lib/models';
+import { AddOrRemoveValueFromArray } from '@/lib/utils/form-utils';
 
 const useStyles = createUseThemedStyles((theme) => ({
 	modal: {
@@ -30,7 +34,6 @@ const useStyles = createUseThemedStyles((theme) => ({
 	searchInput: {
 		flex: 1,
 		display: 'flex',
-		marginRight: 16,
 	},
 	addedHeaderCol: {
 		width: '40%',
@@ -64,6 +67,24 @@ const useStyles = createUseThemedStyles((theme) => ({
 			},
 		},
 	},
+	resourceItem: {
+		display: 'flex',
+		padding: '4px 24px',
+		alignItems: 'center',
+		borderBottom: `1px solid ${theme.colors.border}`,
+		'&:last-of-type': {
+			borderBottom: 0,
+		},
+	},
+	imagePreview: {
+		width: 96,
+		height: 54,
+		flexShrink: 0,
+		marginRight: 16,
+		backgroundSize: 'cover',
+		backgroundPosition: 'center',
+		backgroundRepeat: 'no-repeat',
+	},
 }));
 
 interface SelectResourcesModalProps extends ModalProps {
@@ -72,9 +93,20 @@ interface SelectResourcesModalProps extends ModalProps {
 
 export const SelectResourcesModal = ({ onAdd, ...props }: SelectResourcesModalProps) => {
 	const classes = useStyles();
+	const handleError = useHandleError();
+	const [contents, setContents] = useState<Content[]>([]);
+	const [selectedContentIds, setSelectedContentIds] = useState<string[]>([]);
 
-	const handleOnEnter = () => {
-		// TODO: reset form
+	const handleOnEnter = async () => {
+		setContents([]);
+		setSelectedContentIds([]);
+
+		try {
+			const response = await resourceLibraryService.getResourceLibrary().fetch();
+			setContents(Object.values(response.contentsByTagGroupId).flat());
+		} catch (error) {
+			handleError(error);
+		}
 	};
 
 	const handleOnEntered = () => {
@@ -91,18 +123,18 @@ export const SelectResourcesModal = ({ onAdd, ...props }: SelectResourcesModalPr
 					<div className={classes.resourceHeaderCol}>
 						<InputHelperSearch
 							className={classes.searchInput}
-							onClear={function (event: React.MouseEvent<HTMLButtonElement, MouseEvent>): void {
+							onClear={() => {
 								throw new Error('Function not implemented.');
 							}}
 						/>
 						<MegaFilter
-							className="flex-shrink-0"
+							className="ms-4 flex-shrink-0"
 							displayCount={false}
 							displayFilterIcon
 							buttonTitle="Filter"
 							modalTitle="Filter"
 							value={[]}
-							onChange={function (value: Filter[]): void {
+							onChange={(value) => {
 								throw new Error('Function not implemented.');
 							}}
 						/>
@@ -113,54 +145,39 @@ export const SelectResourcesModal = ({ onAdd, ...props }: SelectResourcesModalPr
 				</div>
 				<div className={classes.bodyInner}>
 					<div className={classes.resourcesCol}>
-						<ul>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
-							<li>1</li>
+						<ul className="list-unstyled m-0">
+							{contents.map((c) => (
+								<li className={classes.resourceItem}>
+									<Form.Check
+										type="checkbox"
+										name="resources"
+										id={`resource--${c.contentId}`}
+										label=""
+										value={c.contentId}
+										checked={selectedContentIds.includes(c.contentId)}
+										onChange={({ currentTarget }) => {
+											setSelectedContentIds(
+												AddOrRemoveValueFromArray(currentTarget.value, selectedContentIds)
+											);
+										}}
+									/>
+									<div
+										className={classes.imagePreview}
+										style={{ backgroundImage: `url(${c.imageUrl})` }}
+									/>
+									<div className="overflow-hidden">
+										<span className="d-block text-truncate">{c.title}</span>
+										<span className="d-block text-truncate">{c.author}</span>
+									</div>
+								</li>
+							))}
 						</ul>
 					</div>
 					<div className={classes.addedCol}>
 						<ul>
-							<li>Resource Title</li>
-							<li>Resource Title</li>
-							<li>Resource Title</li>
+							{selectedContentIds.map((cid) => (
+								<li>Resource Title</li>
+							))}
 						</ul>
 					</div>
 				</div>
@@ -178,7 +195,7 @@ export const SelectResourcesModal = ({ onAdd, ...props }: SelectResourcesModalPr
 							onAdd([]);
 						}}
 					>
-						Add Resources
+						Add Resources ({selectedContentIds.length})
 					</Button>
 				</div>
 			</Modal.Footer>
