e246172408ae4f76e98fde756fa67229f5d4af6b
Adam Grayson <asgrayson90@gmail.com>
2025-02-13 12:07:55 -0500

    live update one col

--- src/components/admin/pages/row-settings-one-column.tsx
+++ src/components/admin/pages/row-settings-one-column.tsx
@@ -1,14 +1,15 @@
 import { v4 as uuidv4 } from 'uuid';
-import React, { useEffect, useMemo, useState } from 'react';
-import { Button, Form } from 'react-bootstrap';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import { Form } from 'react-bootstrap';
+import { OneColumnImageRowModel } from '@/lib/models';
 import { pagesService } from '@/lib/services';
 import useHandleError from '@/hooks/use-handle-error';
+import usePageBuilderContext from '@/hooks/use-page-builder-context';
+import useDebouncedAsyncFunction from '@/hooks/use-debounced-async-function';
 import { CollapseButton } from '@/components/admin/pages/collapse-button';
 import InputHelper from '@/components/input-helper';
 import WysiwygBasic from '@/components/wysiwyg-basic';
 import { AdminFormImageInput } from '@/components/admin/admin-form-image-input';
-import usePageBuilderContext from '@/hooks/use-page-builder-context';
-import { OneColumnImageRowModel } from '@/lib/models';
 
 export const RowSettingsOneColumn = () => {
 	const handleError = useHandleError();
@@ -23,31 +24,34 @@ export const RowSettingsOneColumn = () => {
 	});
 
 	useEffect(() => {
+		if (!oneColumnImageRow) {
+			return;
+		}
+
 		setFormValues({
-			headline: oneColumnImageRow?.columnOne.headline ?? '',
-			description: oneColumnImageRow?.columnOne.description ?? '',
-			imageFileUploadId: oneColumnImageRow?.columnOne.imageFileUploadId ?? '',
-			imageUrl: oneColumnImageRow?.columnOne.imageUrl ?? '',
-			imageAltText: oneColumnImageRow?.columnOne.imageAltText ?? '',
+			headline: oneColumnImageRow.columnOne.headline,
+			description: oneColumnImageRow.columnOne.description,
+			imageFileUploadId: oneColumnImageRow.columnOne.imageFileUploadId,
+			imageUrl: oneColumnImageRow.columnOne.imageUrl,
+			imageAltText: oneColumnImageRow.columnOne.imageAltText,
 		});
 	}, [oneColumnImageRow]);
 
-	const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
+	const debouncedSubmission = useDebouncedAsyncFunction(async (fv: typeof formValues) => {
 		setIsSaving(true);
 
 		try {
-			if (!currentPageRow) {
-				throw new Error('currentPageRow is undefined.');
+			if (!oneColumnImageRow) {
+				throw new Error('oneColumnImageRow is undefined.');
 			}
 
 			const response = await pagesService
-				.updateOneColumnRow(currentPageRow.pageRowId, {
+				.updateOneColumnRow(oneColumnImageRow.pageRowId, {
 					columnOne: {
-						headline: formValues.headline,
-						description: formValues.description,
-						imageFileUploadId: formValues.imageFileUploadId,
-						imageAltText: formValues.imageAltText,
+						headline: fv.headline,
+						description: fv.description,
+						imageFileUploadId: fv.imageFileUploadId,
+						imageAltText: fv.imageAltText,
 					},
 				})
 				.fetch();
@@ -58,48 +62,95 @@ export const RowSettingsOneColumn = () => {
 		} finally {
 			setIsSaving(false);
 		}
-	};
+	});
+
+	const handleInputChange = useCallback(
+		({ currentTarget }: React.ChangeEvent<HTMLInputElement>) => {
+			setFormValues((previousValue) => {
+				const newValue = {
+					...previousValue,
+					[currentTarget.name]: currentTarget.value,
+				};
+
+				debouncedSubmission(newValue);
+				return newValue;
+			});
+		},
+		[debouncedSubmission]
+	);
+
+	const handleQuillChange = useCallback(
+		(value: string) => {
+			setFormValues((previousValue) => {
+				const newValue = {
+					...previousValue,
+					description: value,
+				};
+
+				debouncedSubmission(newValue);
+				return newValue;
+			});
+		},
+		[debouncedSubmission]
+	);
+
+	const handleImageChange = useCallback(
+		async (nextId: string, nextSrc: string) => {
+			setFormValues((previousValue) => ({
+				...previousValue,
+				imageFileUploadId: nextId,
+				imageUrl: nextSrc,
+			}));
+
+			setIsSaving(true);
+
+			try {
+				if (!oneColumnImageRow) {
+					throw new Error('oneColumnImageRow is undefined.');
+				}
+
+				const response = await pagesService
+					.updateOneColumnRow(oneColumnImageRow.pageRowId, {
+						columnOne: {
+							headline: formValues.headline,
+							description: formValues.description,
+							imageFileUploadId: nextId,
+							imageAltText: formValues.imageAltText,
+						},
+					})
+					.fetch();
+
+				updatePageRow(response.pageRow);
+			} catch (error) {
+				handleError(error);
+			} finally {
+				setIsSaving(false);
+			}
+		},
+		[oneColumnImageRow, formValues, handleError, setIsSaving, updatePageRow]
+	);
 
 	return (
-		<Form onSubmit={handleFormSubmit}>
+		<>
 			<CollapseButton title="Item 1" initialShow>
 				<InputHelper
 					className="mb-4"
 					type="text"
 					label="Headline"
+					name="headline"
 					value={formValues.headline}
-					onChange={({ currentTarget }) => {
-						setFormValues((previousValue) => ({
-							...previousValue,
-							headline: currentTarget.value,
-						}));
-					}}
+					onChange={handleInputChange}
 				/>
 				<Form.Group className="mb-4">
 					<Form.Label className="mb-2">Description</Form.Label>
-					<WysiwygBasic
-						height={228}
-						value={formValues.description}
-						onChange={(value) => {
-							setFormValues((previousValue) => ({
-								...previousValue,
-								description: value,
-							}));
-						}}
-					/>
+					<WysiwygBasic height={228} value={formValues.description} onChange={handleQuillChange} />
 				</Form.Group>
 				<Form.Group className="mb-6">
 					<Form.Label className="mb-2">Image</Form.Label>
 					<AdminFormImageInput
 						className="mb-4"
 						imageSrc={formValues.imageUrl}
-						onSrcChange={(nextId, nextSrc) => {
-							setFormValues((previousValue) => ({
-								...previousValue,
-								imageFileUploadId: nextId,
-								imageUrl: nextSrc,
-							}));
-						}}
+						onSrcChange={handleImageChange}
 						presignedUploadGetter={(blob) => {
 							return pagesService.createPresignedFileUpload({
 								contentType: blob.type,
@@ -110,19 +161,12 @@ export const RowSettingsOneColumn = () => {
 					<InputHelper
 						type="text"
 						label="Image alt text"
+						name="imageAltText"
 						value={formValues.imageAltText}
-						onChange={({ currentTarget }) => {
-							setFormValues((previousValue) => ({
-								...previousValue,
-								imageAltText: currentTarget.value,
-							}));
-						}}
+						onChange={handleInputChange}
 					/>
 				</Form.Group>
 			</CollapseButton>
-			<Button type="submit" variant="warning">
-				Temp Submit Button (No live saving yet)
-			</Button>
-		</Form>
+		</>
 	);
 };
