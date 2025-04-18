0b916ed36c2cd7a1972ac92f96cd06c8d1baa34c
Adam Grayson <asgrayson90@gmail.com>
2025-04-18 11:15:21 -0400

    course unit layout updates

--- src/components/courses/index.ts
+++ src/components/courses/index.ts
@@ -2,6 +2,6 @@ export * from './course-downloadable';
 export * from './course-module';
 export * from './course-unit-available';
 export * from './course-unit-complete';
-export * from './course-unit-header';
+export * from './course-unit-layout';
 export * from './course-unit-locked';
 export * from './course-video';
--- src/routes/courses/course-units.tsx
+++ src/routes/courses/course-units.tsx
@@ -2,7 +2,6 @@ import React, { useCallback, useMemo, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { Helmet } from 'react-helmet';
-import classNames from 'classnames';
 import {
 	getFirstUnlockedAndIncompleteCourseUnitIdByCourseSession,
 	getNextIncompleteAndNotStronglyLockedCourseUnitIdByCourseSession,
@@ -27,76 +26,21 @@ import {
 	CourseModule,
 	CourseUnitAvailable,
 	CourseUnitComplete,
-	CourseUnitHeader,
+	CourseUnitLayout,
 	CourseUnitLocked,
 } from '@/components/courses';
-import { createUseThemedStyles } from '@/jss/theme';
-import mediaQueries from '@/jss/media-queries';
-import { ReactComponent as CloseIcon } from '@/assets/icons/icon-close.svg';
 import { ReactComponent as MenuIcon } from '@/assets/icons/icon-menu.svg';
-
-const headerHeight = 60;
-const asideWidth = 344;
-
-const useStyles = createUseThemedStyles((theme) => ({
-	wrapper: {
-		top: 0,
-		left: 0,
-		right: 0,
-		bottom: 0,
-		zIndex: 0,
-		position: 'fixed',
-	},
-	aside: {
-		top: headerHeight,
-		left: 0,
-		bottom: 0,
-		zIndex: 4,
-		width: asideWidth,
-		overflowY: 'auto',
-		padding: '24px 16px',
-		position: 'absolute',
-		transition: '200ms transform',
-		backgroundColor: theme.colors.n0,
-		borderRight: `1px solid ${theme.colors.n100}`,
-		[mediaQueries.lg]: {
-			top: 0,
-			maxWidth: '100%',
-			transform: 'translateX(-100%)',
-			'&.show': {
-				transform: 'translateX(0%)',
-			},
-		},
-	},
-	previewPane: {
-		top: headerHeight,
-		left: asideWidth,
-		right: 0,
-		bottom: 0,
-		zIndex: 0,
-		overflowY: 'auto',
-		padding: '40px 24px',
-		position: 'absolute',
-		transition: '200ms left',
-		backgroundColor: theme.colors.n75,
-		[mediaQueries.lg]: {
-			left: 0,
-			padding: '24px 4px',
-		},
-	},
-}));
 
 export async function loader() {
 	return null;
 }
 
 export const Component = () => {
-	const classes = useStyles();
 	const handleError = useHandleError();
 	const { courseIdentifier, unitId } = useParams<{ courseIdentifier: string; unitId: string }>();
 	const navigate = useNavigate();
 
-	const [showAside, setShowAside] = useState(false);
+	const [showMenu, setShowMenu] = useState(false);
 	const [course, setCourse] = useState<CourseModel>();
 	const [requiredModules, setRequiredModules] = useState<CourseModuleModel[]>([]);
 	const [optionalModules, setOptionalModules] = useState<CourseModuleModel[]>([]);
@@ -279,184 +223,181 @@ export const Component = () => {
 			<Helmet>
 				<title>Cobalt | Courses - Session</title>
 			</Helmet>
-			<div className={classes.wrapper}>
-				{course && <CourseUnitHeader height={headerHeight} course={course} />}
-				<div
-					className={classNames(classes.aside, {
-						show: showAside,
-					})}
-				>
-					<div className="d-lg-none mb-4 d-flex justify-content-end">
-						<Button
-							variant="link"
-							className="d-flex align-items-center text-decoration-none"
-							onClick={() => {
-								setShowAside((previousValue) => !previousValue);
-							}}
-						>
-							<CloseIcon width={16} height={16} className="me-1" />
-							Close
-						</Button>
-					</div>
-					{course &&
-						requiredModules.map((courseModule) => (
-							<CourseModule
-								compact
-								activeCourseUnitId={unitId}
-								key={courseModule.courseModuleId}
-								courseModule={courseModule}
-								courseSessionUnitStatusIdsByCourseUnitId={
-									course.currentCourseSession
-										? course.currentCourseSession.courseSessionUnitStatusIdsByCourseUnitId
-										: {}
-								}
-								courseUnitLockStatusesByCourseUnitId={
-									course.currentCourseSession
-										? course.currentCourseSession.courseUnitLockStatusesByCourseUnitId
-										: course.defaultCourseUnitLockStatusesByCourseUnitId
-								}
-								onCourseUnitClick={(desiredCourseUnit) => {
-									navigate(
-										`/courses/${course.urlName}/course-units/${desiredCourseUnit.courseUnitId}`
-									);
 
-									if (!courseUnit) {
-										return;
+			<CourseUnitLayout
+				title={course?.title}
+				showMenu={showMenu}
+				onShowMenuToggle={setShowMenu}
+				onExitButtonClick={() => {
+					navigate(`/courses/${course?.urlName}`);
+				}}
+				onNeedHelpButtonClick={() => {
+					navigate('/feedback');
+				}}
+				menuElement={
+					<>
+						{course &&
+							requiredModules.map((courseModule) => (
+								<CourseModule
+									compact
+									activeCourseUnitId={unitId}
+									key={courseModule.courseModuleId}
+									courseModule={courseModule}
+									courseSessionUnitStatusIdsByCourseUnitId={
+										course.currentCourseSession
+											? course.currentCourseSession.courseSessionUnitStatusIdsByCourseUnitId
+											: {}
 									}
+									courseUnitLockStatusesByCourseUnitId={
+										course.currentCourseSession
+											? course.currentCourseSession.courseUnitLockStatusesByCourseUnitId
+											: course.defaultCourseUnitLockStatusesByCourseUnitId
+									}
+									onCourseUnitClick={(desiredCourseUnit) => {
+										navigate(
+											`/courses/${course.urlName}/course-units/${desiredCourseUnit.courseUnitId}`
+										);
 
-									analyticsService.persistEvent(AnalyticsNativeEventTypeId.CLICKTHROUGH_COURSE_UNIT, {
-										courseId: course.courseId,
-										...(course.currentCourseSession && {
-											courseSessionId: course.currentCourseSession.courseSessionId,
-										}),
-										courseUnitId: desiredCourseUnit.courseUnitId,
-										source: 'COURSE_UNIT',
-										sourceCourseUnitId: courseUnit.courseUnitId,
-									});
-								}}
-							/>
-						))}
-					{optionalModules.length > 0 && (
-						<>
-							<hr className="my-4" />
-							<h6 className="px-4 pt-4 pb-1 small fw-bold text-uppercase text-n500">Optional Modules</h6>
-							{course &&
-								optionalModules.map((courseModule) => (
-									<CourseModule
-										compact
-										activeCourseUnitId={unitId}
-										key={courseModule.courseModuleId}
-										courseModule={courseModule}
-										courseSessionUnitStatusIdsByCourseUnitId={
-											course.currentCourseSession
-												? course.currentCourseSession.courseSessionUnitStatusIdsByCourseUnitId
-												: {}
+										if (!courseUnit) {
+											return;
 										}
-										courseUnitLockStatusesByCourseUnitId={
-											course.currentCourseSession
-												? course.currentCourseSession.courseUnitLockStatusesByCourseUnitId
-												: course.defaultCourseUnitLockStatusesByCourseUnitId
-										}
-										onCourseUnitClick={(desiredCourseUnit) => {
-											navigate(
-												`/courses/${course.urlName}/course-units/${desiredCourseUnit.courseUnitId}`
-											);
 
-											if (!courseUnit) {
-												return;
+										analyticsService.persistEvent(
+											AnalyticsNativeEventTypeId.CLICKTHROUGH_COURSE_UNIT,
+											{
+												courseId: course.courseId,
+												...(course.currentCourseSession && {
+													courseSessionId: course.currentCourseSession.courseSessionId,
+												}),
+												courseUnitId: desiredCourseUnit.courseUnitId,
+												source: 'COURSE_UNIT',
+												sourceCourseUnitId: courseUnit.courseUnitId,
 											}
+										);
+									}}
+								/>
+							))}
+						{optionalModules.length > 0 && (
+							<>
+								<hr className="my-4" />
+								<h6 className="px-4 pt-4 pb-1 small fw-bold text-uppercase text-n500">
+									Optional Modules
+								</h6>
+								{course &&
+									optionalModules.map((courseModule) => (
+										<CourseModule
+											compact
+											activeCourseUnitId={unitId}
+											key={courseModule.courseModuleId}
+											courseModule={courseModule}
+											courseSessionUnitStatusIdsByCourseUnitId={
+												course.currentCourseSession
+													? course.currentCourseSession
+															.courseSessionUnitStatusIdsByCourseUnitId
+													: {}
+											}
+											courseUnitLockStatusesByCourseUnitId={
+												course.currentCourseSession
+													? course.currentCourseSession.courseUnitLockStatusesByCourseUnitId
+													: course.defaultCourseUnitLockStatusesByCourseUnitId
+											}
+											onCourseUnitClick={(desiredCourseUnit) => {
+												navigate(
+													`/courses/${course.urlName}/course-units/${desiredCourseUnit.courseUnitId}`
+												);
 
-											analyticsService.persistEvent(
-												AnalyticsNativeEventTypeId.CLICKTHROUGH_COURSE_UNIT,
-												{
-													courseId: course.courseId,
-													...(course.currentCourseSession && {
-														courseSessionId: course.currentCourseSession.courseSessionId,
-													}),
-													courseUnitId: desiredCourseUnit.courseUnitId,
-													source: 'COURSE_UNIT',
-													sourceCourseUnitId: courseUnit.courseUnitId,
+												if (!courseUnit) {
+													return;
 												}
-											);
-										}}
-									/>
-								))}
-						</>
-					)}
-				</div>
-				<div className={classes.previewPane}>
-					<AsyncWrapper fetchData={fetchData}>
-						<Container>
-							<Row className="d-block d-lg-none mb-6">
-								<Col lg={12} xl={{ offset: 1, span: 10 }} xxl={{ offset: 2, span: 8 }}>
-									<Button
-										variant="link"
-										className="px-0 d-flex align-items-center text-decoration-none"
-										onClick={() => {
-											setShowAside((previousValue) => !previousValue);
-										}}
-									>
-										<MenuIcon className="me-1" />
-										Course Content
-									</Button>
-								</Col>
-							</Row>
-							<Row>
-								<Col lg={12} xl={{ offset: 1, span: 10 }} xxl={{ offset: 2, span: 8 }}>
-									{courseUnitCompleted ? (
-										<>
-											{courseUnit && (
-												<CourseUnitComplete
-													courseUnit={courseUnit}
-													onRestartActivityButtonClick={() => {
-														setCourseUnitCompleted(false);
-													}}
-													onNextButtonClick={handleCompletedUnitNextButtonClick}
-												/>
-											)}
-										</>
-									) : (
-										<>
-											{courseUnitIsStronglyLocked ? (
-												<>
-													{course && courseUnit && (
-														<CourseUnitLocked
-															courseUrlName={course.urlName}
-															courseUnit={courseUnit}
-															dependencyCourseUnits={strongCourseUnitDependencies}
-														/>
-													)}
-												</>
-											) : (
-												<>
-													{course && course.currentCourseSession && courseUnit && (
-														<CourseUnitAvailable
-															courseUrlName={course.urlName}
-															courseSessionId={
-																course.currentCourseSession?.courseSessionId
-															}
-															courseVideos={course.videos}
-															courseUnit={courseUnit}
-															dependencyCourseUnits={weakCourseUnitDependencies}
-															onActivityComplete={handleActivityComplete}
-															onSkipActivityButtonClick={handleSkipActivityButtonClick}
-															onNextButtonClick={handleCompletedUnitNextButtonClick}
-															onView={handleCourseUnitView}
-															onCompletionThresholdPassed={
-																handleCompletionThresholdPassed
-															}
-														/>
-													)}
-												</>
-											)}
-										</>
-									)}
-								</Col>
-							</Row>
-						</Container>
-					</AsyncWrapper>
-				</div>
-			</div>
+
+												analyticsService.persistEvent(
+													AnalyticsNativeEventTypeId.CLICKTHROUGH_COURSE_UNIT,
+													{
+														courseId: course.courseId,
+														...(course.currentCourseSession && {
+															courseSessionId:
+																course.currentCourseSession.courseSessionId,
+														}),
+														courseUnitId: desiredCourseUnit.courseUnitId,
+														source: 'COURSE_UNIT',
+														sourceCourseUnitId: courseUnit.courseUnitId,
+													}
+												);
+											}}
+										/>
+									))}
+							</>
+						)}
+					</>
+				}
+			>
+				<AsyncWrapper fetchData={fetchData}>
+					<Container>
+						<Row className="d-block d-lg-none mb-6">
+							<Col lg={12} xl={{ offset: 1, span: 10 }} xxl={{ offset: 2, span: 8 }}>
+								<Button
+									variant="link"
+									className="px-0 d-flex align-items-center text-decoration-none"
+									onClick={() => {
+										setShowMenu(true);
+									}}
+								>
+									<MenuIcon className="me-1 flex-shrink-0" />
+									Course Content
+								</Button>
+							</Col>
+						</Row>
+						<Row>
+							<Col lg={12} xl={{ offset: 1, span: 10 }} xxl={{ offset: 2, span: 8 }}>
+								{courseUnitCompleted ? (
+									<>
+										{courseUnit && (
+											<CourseUnitComplete
+												courseUnit={courseUnit}
+												onRestartActivityButtonClick={() => {
+													setCourseUnitCompleted(false);
+												}}
+												onNextButtonClick={handleCompletedUnitNextButtonClick}
+											/>
+										)}
+									</>
+								) : (
+									<>
+										{courseUnitIsStronglyLocked ? (
+											<>
+												{course && courseUnit && (
+													<CourseUnitLocked
+														courseUrlName={course.urlName}
+														courseUnit={courseUnit}
+														dependencyCourseUnits={strongCourseUnitDependencies}
+													/>
+												)}
+											</>
+										) : (
+											<>
+												{course && course.currentCourseSession && courseUnit && (
+													<CourseUnitAvailable
+														courseUrlName={course.urlName}
+														courseSessionId={course.currentCourseSession?.courseSessionId}
+														courseVideos={course.videos}
+														courseUnit={courseUnit}
+														dependencyCourseUnits={weakCourseUnitDependencies}
+														onActivityComplete={handleActivityComplete}
+														onSkipActivityButtonClick={handleSkipActivityButtonClick}
+														onNextButtonClick={handleCompletedUnitNextButtonClick}
+														onView={handleCourseUnitView}
+														onCompletionThresholdPassed={handleCompletionThresholdPassed}
+													/>
+												)}
+											</>
+										)}
+									</>
+								)}
+							</Col>
+						</Row>
+					</Container>
+				</AsyncWrapper>
+			</CourseUnitLayout>
 		</>
 	);
 };
