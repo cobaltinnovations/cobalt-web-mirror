79cfc52019de915c732a1d733b1ec655cee9ba6e
Adam Grayson <asgrayson90@gmail.com>
2025-06-18 14:54:39 -0400

    footerNav featureFlag, homeHero data driven

--- src/components/home-hero.tsx
+++ src/components/home-hero.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 
+import useAccount from '@/hooks/use-account';
 import HeroContainer from '@/components/hero-container';
+import PageHeader from './page-header';
 
 const HomeHero = () => {
+	const { institution } = useAccount();
 	return (
-		<HeroContainer>
-			<h1 className="mb-3 text-center">Recommended for you</h1>
-			<p className="mb-5 text-center">
-				Peers, Coping First Aid Coaches, Therapists, Psychiatrists, and more are here to help
-			</p>
-		</HeroContainer>
+		<>
+			{institution.heroImageUrl ? (
+				<PageHeader
+					className="bg-p50"
+					title={<h1 className="mb-0">{institution.heroTitle ?? 'Recommended for you'}</h1>}
+					descriptionHtml={`<p className="mb-0">
+							${
+								institution.heroDescription ??
+								'Peers, Coping First Aid Coaches, Therapists, Psychiatrists, and more are here to help'
+							}
+						</p>`}
+					imageUrl={institution.heroImageUrl}
+					imageAlt={institution.heroTitle ?? 'Recommended for you'}
+				/>
+			) : (
+				<HeroContainer>
+					<h1 className="mb-3 text-center">{institution.heroTitle ?? 'Recommended for you'}</h1>
+					<p className="mb-5 text-center">
+						{institution.heroDescription ??
+							'Peers, Coping First Aid Coaches, Therapists, Psychiatrists, and more are here to help'}
+					</p>
+				</HeroContainer>
+			)}
+		</>
 	);
 };
 
